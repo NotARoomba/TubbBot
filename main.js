b6b0a1f51525604a30bf00e24bd60425ae0237ad
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-03 10:33:06 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-const prefix = '*'
+const prefix = '-'
 
 const fs = require('fs');
 const { execute } = require('./commands/ping');
