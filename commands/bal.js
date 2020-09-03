1f7afe6f0bf6a8161ccc1e2b411bce7ce1ec3d89
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-03 13:12:22 -0500

    Update bal.js

--- commands/bal.js
+++ commands/bal.js
@@ -26,7 +26,7 @@ module.exports = {
             });
         } 
 
-        return message.channel.send(`${client.users.cache.get(user.id).username} has $${money[user.id].money}.`);
+        return message.channel.send(`${client.users.cache.get(user.id).users} has $${money[user.id].money}.`);
 
     }
 
