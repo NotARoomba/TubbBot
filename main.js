22cd3aece2d0684edb31d7fec4abce0eb48b2b31
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-03 12:13:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ client.on('guildMemberAdd', member => {
 const welcomeEmbed = new Discord.MessageEmbed()
     .setColor('#8B0000')
     .setTitle(`Welcome`)
-    .setDescription(`Welcome to Corona, ${member}! Once you join, you will be infected with the corona virus. There are 3 stages, diagnosed, recovering, and recovered. Once recovered you will be free just to hang out on this server and play, but until then, you have a hard battle. After you recovered, you will be vaccinated which will make sure you're never infected ever again. The way you level up and get through the virus is simply by just hanging out and "spreading" the word about this server! glhf and may the odds be ever in your favor.`)
+    .setDescription(`Welcome to Corona, ${member}! Once you join, you will be infected with the corona virus. There are 3 stages, diagnosed, recovering, and recovered. Once recovered you will be free just to hang out on this server and play, but until then, you have a hard battle. After you recovered, you will be vaccinated which will make sure you're never infected ever again. The way you level up and get through the virus is simply by just hanging out and "spreading" the word about this server! Goodluck and may the odds be ever in your favor.`)
 
 channel.send(welcomeEmbed);
 
