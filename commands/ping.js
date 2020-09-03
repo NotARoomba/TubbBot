27d6d80e2b6a2cf7af765e8b46d87e9711b9ae50
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-02 22:43:37 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'ping',
     description: "this is a ping command!",
     execute(message, args){
-        message.reply('Finding ping to bot...!').then((resultMessage) => {
+        then((resultMessage) => {
             message.reply(`Your ping is ${Date.now() - message.createdTimestamp} ms`)
             target=_blank
         })
