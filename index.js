4560eae428ffe4f8977709302dc0e636a771e018
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-03 18:36:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
 const command = require('./command.js')
+const economy = require('../../economy')
 
 client.on('ready', () => {
   console.log('Tubb is online!')
