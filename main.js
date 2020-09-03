d2f655b6298fc6085037e64a33914275bd5f5acd
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-02 22:37:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -25,9 +25,8 @@ client.on('guildMemberAdd', member => {
     if(!channel) return;
 
 const welcomeEmbed = new Discord.MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle(`Welcome ${member}`)
-    .setAuthor(`TubbBot`)
+    .setColor('#8B0000')
+    .setTitle(`Welcome`)
     .setDescription(`Welcome to Corona, ${member}! Once you join, you will be infected with the corona virus. There are 3 stages, diagnosed, recovering, and recovered. Once recovered you will be free just to hang out on this server and play, but until then, you have a hard battle. After you recovered, you will be vaccinated which will make sure you're never infected ever again. The way you level up and get through the virus is simply by just hanging out and "spreading" the word about this server! glhf and may the odds be ever in your favor.`)
 
 channel.send(welcomeEmbed);
