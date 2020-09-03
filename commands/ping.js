6c584fa29f6b8a4198232aff7c0ffad69551957c
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-03 10:14:20 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,8 +4,14 @@ module.exports = {
     name: 'ping',
     description: "this is a ping command!",
     execute(message, args){
-        message.reply((`Finding ping to bot!...`)).then((resultMessage) => {
-            message.reply((`Your ping is ${Date.now() - message.createdTimestamp} ms`))
+        
+const pingEmbed = new Discord.MessageEmbed()
+    .setColor('#8B0000')
+    .setTitle(`Ping`)
+    .setDescription(`Finding ping to bot!...`)
+        
+        channel.send(pingEmbed).then((resultMessage) => {
+            message.reply(`Your ping is ${Date.now() - message.createdTimestamp} ms`)
             target=_blank
         })
     }
