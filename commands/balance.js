a0871e70bd4418a72f85b8e4b2e67d50c4523d17
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-03 12:31:20 -0500

    Money system!

---
+++ commands/balance.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const money = require("../money.json");
+
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+module.exports = {
+    name: 'bal, money',
+    description: "this is the balance command!",
+    execute(message, args){
+
+        if(!args[0]) {
+            var user = message.author;
+        } else {
+            var user = message.mentions.users.first() || client.users.get(args[0]);
+        }
+
+        if(!money[user.id]) {
+            money[user.id] = {
+                name: client.users.get(user.id).tag,
+                money: 0
+            }
+            fs.writeFile("./money.json", JSON.stringify(money), (err) => {
+                if(err) console.log(err);
+            });
+        } 
+
+        return message.channel.send(`${bot.user.get(user.id).username} has $${money[user.id].money}.`);
+
+    }
+
+};
