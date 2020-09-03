ab54d3cd7c291198634661a86418c04a580c756a
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-03 10:11:28 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,18 +1,10 @@
-const Discord = require('discord.js');
-
-const client = new Discord.Client();
+const { Channel } = require("discord.js");
 
 module.exports = {
     name: 'ping',
     description: "this is a ping command!",
     execute(message, args){
-        
-const pingEmbed = new Discord.MessageEmbed()
-    .setColor('#8B0000')
-    .setTitle(`Ping`)
-    .setDescription(`Finding ping to bot!...`)
-        
-        message.reply(pingEmbed).then((resultMessage) => {
+        message.reply((`Finding ping to bot!...`)).then((resultMessage) => {
             message.reply(`Your ping is ${Date.now() - message.createdTimestamp} ms`)
             target=_blank
         })
