fffff70c8047e5c5f28bd0f0b07ce46263c23843
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-03 17:51:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,23 +6,6 @@ client.on('ready', () => {
   console.log('Tubb is online!')
 });
 
-const baseFile = 'command-base.js'
-  const commandBase = require(`./commands/${baseFile}`)
-
-  const readCommands = (dir) => {
-    const files = fs.readdirSync(path.join(__dirname, dir))
-    for (const file of files) {
-      const stat = fs.lstatSync(path.join(__dirname, dir, file))
-      if (stat.isDirectory()) {
-        readCommands(path.join(dir, file))
-      } else if (file !== baseFile) {
-        const option = require(path.join(__dirname, dir, file))
-        commandBase(client, option)
-      }
-    }
-  }
-
-  readCommands('commands')
 
 
 client.user.setActivity('|-help|');
