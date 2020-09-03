ee040bce2562bba19f312a6f270aa8593e01baf7
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-03 18:07:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 const config = require('./config.json')
-
+const command = require('./command.js')
 client.on('ready', () => {
   console.log('Tubb is online!')
   client.user.setActivity('|-help|');
