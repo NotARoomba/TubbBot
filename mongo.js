3b192c3a0de2062a2f63fbe584fec4837722bce0
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-03 18:49:59 -0500

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const { mongoPath } = require('./config.json')
 
 module.exports = async () => {
-  await mongoose.connect(mongoPath, {
+  await mongoose.create.connection(mongoPath, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
