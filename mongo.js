d9a219d326dd7c64f41789a0d92965396b0c6955
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-04 16:03:42 -0500

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = async () => {
-  await mongoose.connect('mongodb://localhost:27017/data', {
+  await mongoose.connect('mongodb+srv://L061571C5:Joeblow6@tubbbot.kfqqn.mongodb.net/data?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
