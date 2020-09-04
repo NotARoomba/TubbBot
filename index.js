3cde951c0d392e8651bc3c277126cddd57017175
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-04 09:35:50 -0500

    stuff

--- index.js
+++ index.js
@@ -16,7 +16,23 @@ client.on('ready', async () => {
       mongoose.connection.close()
     }
   })
+  const baseFile = 'command-base.js'
+  const commandBase = require(`./commands/${baseFile}`)
 
+  const readCommands = (dir) => {
+    const files = fs.readdirSync(path.join(__dirname, dir))
+    for (const file of files) {
+      const stat = fs.lstatSync(path.join(__dirname, dir, file))
+      if (stat.isDirectory()) {
+        readCommands(path.join(dir, file))
+      } else if (file !== baseFile) {
+        const option = require(path.join(__dirname, dir, file))
+        commandBase(client, option)
+      }
+    }
+  }
+
+  readCommands('commands')
 });
 
 
