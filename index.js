9aa56249080713e22deca56e3e7ce0da5f58d668
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-04 15:26:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const config = require('./config.json')
 const command = require('./command.js')
 const economy = require('./economy')
 const mongo = require('./mongo')
