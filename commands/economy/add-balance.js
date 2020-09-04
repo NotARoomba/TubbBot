6d255eb78961e99220e42ab489fb36f592be03ce
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-04 16:46:11 -0500

    Update add-balance.js

--- commands/economy/add-balance.js
+++ commands/economy/add-balance.js
@@ -9,12 +9,11 @@ module.exports = {
   permissions: 'ADMINISTRATOR',
   callback: async (message, arguments) => {
     const mention = message.mentions.users.first() || message.author
+    const targetId = target.id
 
-    if (!mention) {
-      message.reply('Please tag a user to add coins to.')
-      return
-    }
-
+    const guildId = message.guild.id
+    const userId = target.id
+    
     const coins = arguments[1]
     if (isNaN(coins)) {
       message.reply('Please provide a valid number of coins.')
