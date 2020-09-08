761b57a5c01149609e716a12baa68825ca176b8d
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-08 11:24:50 -0500

    Update welcome.js

--- features/features/welcome.js
+++ features/features/welcome.js
@@ -3,16 +3,8 @@ const command = require('@util/command')
 const welcomeSchema = require('@schemas/welcome-schema')
 
 module.exports = (client) => {
-  !setwelcome 
-
-  const welcomeEmbed = new Discord.MessageEmbed()
-    .setColor('#8B0000')
-    .setTitle(`Welcome`)
-    .setDescription(`Welcome to Corona, ${member}! Once you join, you will be infected with the corona virus. There are 3 stages, diagnosed, recovering, and recovered. Once recovered you will be free just to hang out on this server and play, but until then, you have a hard battle. After you recovered, you will be vaccinated which will make sure you're never infected ever again. The way you level up and get through the virus is simply by just hanging out and "spreading" the word about this server! Please read #rules and #roles. Goodluck and may the odds be ever in your favor.(Btw -help)`)
-
-channel.send(welcomeEmbed);
-
-  const cache = {} // guildId: [channelId, text]
+  //!setwelcome 
+    const cache = {} // guildId: [channelId, text]
 
   command(client, 'setwelcome', async (message) => {
     const { member, channel, content, guild } = message
@@ -27,7 +19,11 @@ channel.send(welcomeEmbed);
     const split = text.split(' ')
 
     if (split.length < 2) {
-      channel.send('Please provide a welcome message')
+        const welcomeEmbed = new Discord.MessageEmbed()
+        .setColor('#8B0000')
+        .setTitle(`Welcome`)
+        .setDescription(`Welcome to Corona, ${member}! Once you join, you will be infected with the corona virus. There are 3 stages, diagnosed, recovering, and recovered. Once recovered you will be free just to hang out on this server and play, but until then, you have a hard battle. After you recovered, you will be vaccinated which will make sure you're never infected ever again. The way you level up and get through the virus is simply by just hanging out and "spreading" the word about this server! Please read #rules and #roles. Goodluck and may the odds be ever in your favor.(Btw -help)`)
+        channel.send(welcomeEmbed);
       return
     }
 
