ea4474d0e8dea5f3a0631ee4842fb1ca5cb0960f
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-08 15:01:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const mongo = require('@util/mongo');
 const fs = require('fs');
 const path = require('path');
 const welcome = require('@features/welcome');
-const commandBase = require('./commands/command-base');
 //const Commando = require('discord.js-commando');
 const prefix = "-"
 client.commands = new Discord.Collection();
@@ -35,8 +34,24 @@ client.on('ready', async () => {
   welcome(client)
   loadCommands(client)
   loadFeatures(client)
-  commandBase.loadPrefixes(client)
-  
+
+  const baseFile = 'command-base.js'
+  const commandBase = require(`@commands/commands/${baseFile}`)
+
+  const readCommands = (dir) => {
+    const files = fs.readdirSync(path.join(__dirname, dir))
+    for (const file of files) {
+      const stat = fs.lstatSync(path.join(__dirname, dir, file))
+      if (stat.isDirectory()) {
+        readCommands(path.join(dir, file))
+      } else if (file !== baseFile) {
+        const option = require(path.join(__dirname, dir, file))
+        commandBase(client, option)
+      }
+    }
+  }
+
+  readCommands('commands')
 
 });
 
