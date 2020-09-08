ef33208e254c3b3d2e92521adf4380f5e06dee04
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-08 11:49:35 -0500

    Update welcome.js

--- features/features/welcome.js
+++ features/features/welcome.js
@@ -1,16 +1,15 @@
 module.exports = (client) => {
+    const channelId = '750766854360006676' // welcome channel
+    const targetChannelId = '750556623348564038'  // rules and info
+    const targetChannelId2 = '751559736096456885'
     
-    client.on('guildMemberAdd', member => {
-        const channel = member.guild.channels.cache.find(channel => channel.name === "welcome");
-        if(!channel) return;
+    client.on('guildMemberAdd', (member) => {
+          const welcomeEmbed = new Discord.MessageEmbed()
+            .setColor('#8B0000')
+            .setTitle(`Welcome`)
+            .setDescription(`Welcome to Corona, ${member}! Once you join, you will be infected with the corona virus. There are 3 stages, diagnosed, recovering, and recovered. Once recovered you will be free just to hang out on this server and play, but until then, you have a hard battle. After you recovered, you will be vaccinated which will make sure you're never infected ever again. The way you level up and get through the virus is simply by just hanging out and "spreading" the word about this server! Please read ${member.guild.channels.cache.get(targetChannelId,).toString()}. Goodluck and may the odds be ever in your favor.(Btw -help)`)
     
-    const welcomeEmbed = new Discord.MessageEmbed()
-        .setColor('#8B0000')
-        .setTitle(`Welcome`)
-        .setDescription(`Welcome to Corona, ${member}! Once you join, you will be infected with the corona virus. There are 3 stages, diagnosed, recovering, and recovered. Once recovered you will be free just to hang out on this server and play, but until then, you have a hard battle. After you recovered, you will be vaccinated which will make sure you're never infected ever again. The way you level up and get through the virus is simply by just hanging out and "spreading" the word about this server! Please read #rules and #roles. Goodluck and may the odds be ever in your favor.(Btw -help)`)
-    
-    channel.send(welcomeEmbed);
-    
-    });
-  };
-  
+        const channel = member.guild.channels.cache.get(channelId)
+        channel.send(welcomeEmbed)
+      })
+  }
