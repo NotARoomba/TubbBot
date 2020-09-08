de9abff51c22174e73abf1d982242e36e773c52a
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-08 18:38:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -38,25 +38,7 @@ client.on('ready', async () => {
   loadCommands(client)
   loadFeatures(client)
 
-  const baseFile = 'command-base.js'
-  const commandBase = require(`@commands/commands/${baseFile}`)
-
-  const readCommands = (dir) => {
-    const files = fs.readdirSync(path.join(__dirname, dir))
-    for (const file of files) {
-      const stat = fs.lstatSync(path.join(__dirname, dir, file))
-      if (stat.isDirectory()) {
-        readCommands(path.join(dir, file))
-      } else if (file !== baseFile) {
-        const option = require(path.join(__dirname, dir, file))
-        commandBase(client, option)
-      }
-    }
-  }
-
-  readCommands('commands')
-
-});
+  });
 
 
 
