ccf734d003dbec7b04746fc009abd0c9efef3e76
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-08 12:06:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -56,18 +56,18 @@ client.on('ready', async () => {
 
 
 
-client.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.cache.find(channel => channel.name === "welcome");
-    if(!channel) return;
+//client.on('guildMemberAdd', member => {
+    //const channel = member.guild.channels.cache.find(channel => channel.name === "welcome");
+    //if(!channel) return;
 
-const welcomeEmbed = new Discord.MessageEmbed()
-    .setColor('#8B0000')
-    .setTitle(`Welcome`)
-    .setDescription(`Welcome to Corona, ${member}! Once you join, you will be infected with the corona virus. There are 3 stages, diagnosed, recovering, and recovered. Once recovered you will be free just to hang out on this server and play, but until then, you have a hard battle. After you recovered, you will be vaccinated which will make sure you're never infected ever again. The way you level up and get through the virus is simply by just hanging out and "spreading" the word about this server! Please read #rules and #roles. Goodluck and may the odds be ever in your favor.(Btw -help)`)
+//const welcomeEmbed = new Discord.MessageEmbed()
+    //.setColor('#8B0000')
+    //.setTitle(`Welcome`)
+    //.setDescription(`Welcome to Corona, ${member}! Once you join, you will be infected with the corona virus. There are 3 stages, diagnosed, recovering, and recovered. Once recovered you will be free just to hang out on this server and play, but until then, you have a hard battle. After you recovered, you will be vaccinated which will make sure you're never infected ever again. The way you level up and get through the virus is simply by just hanging out and "spreading" the word about this server! Please read #rules and #roles. Goodluck and may the odds be ever in your favor.(Btw -help)`)
 
-channel.send(welcomeEmbed);
+//channel.send(welcomeEmbed);
 
-});
+//});
 
 
 
