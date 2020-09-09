bac976126b02c8f451ecca1872d28629f15a89f4
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-08 20:10:32 -0500

    Update summon.js

--- commands/commands/summon.js
+++ commands/commands/summon.js
@@ -6,11 +6,12 @@ module.exports = {
     minArgs: 0,
     maxArgs: 0,
     callback: (message, arguments, text) => {
+        
         const summonEmbed = new Discord.MessageEmbed()
         .setColor('#9400D3')
         .setTitle(`Ritual of Kys`)
         .setDescription('@427630841045319701')
 
-    message.reply(summonEmbed)  
+    message.send(summonEmbed)  
     }
 }
