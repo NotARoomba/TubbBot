ac8a7b92cc937ec4c27f563a06908f1be8c0e1b5
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-08 20:40:52 -0500

    Update summon.js

--- commands/commands/summon.js
+++ commands/commands/summon.js
@@ -1,25 +1,39 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const summoned = '427630841045319701'
+//const summoned = '427630841045319701'
 
 module.exports = {
     commands: 'summon',
-    minArgs: 0,
-    maxArgs: 0,
+    maxArgs: 1,
+    expectedArgs: "[Target user's @]",
     permissions: 'ADMINISTRATOR',
     callback: (message, arguments, text) => {
-        
+        const target = message.mentions.users.first() || message.author
+    const targetId = target.id
+
+    const guildId = message.guild.id
+    const userId = target.id
+      
+      const balusrEmbed = new Discord.MessageEmbed()
+            .setColor('#FFFF00')
+            .setTitle(`Error`)
+            .setDescription('Please tag a user to SUMMON.')
+
+      
+      message.reply(balusrEmbed)
+  
+
         const summonEmbed = new Discord.MessageEmbed()
         .setColor('#9400D3')
         .setTitle(`Ritual of Kys`)
-        .setDescription(`<@${summoned}>`)
+        .setDescription(`<@${userId}>`)
         
 
     message.reply(summonEmbed)  
 
     const summoningEmbed = new Discord.MessageEmbed()
         .setColor('#9400D3')
-        .setDescription(`<@${summoned}>`)
+        .setDescription(`<@${userId}>`)
         
 
     message.reply(summoningEmbed)
