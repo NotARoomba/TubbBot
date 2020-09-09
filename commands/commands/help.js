04feb4058cd15ae087d880a381aeda9dd0102e51
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-08 19:15:54 -0500

    Update help.js

--- commands/commands/help.js
+++ commands/commands/help.js
@@ -1,29 +1,58 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const loadCommands = require('./load-commands')
+const { prefix } = require('../config.json')
 
 module.exports = {
-    commands: 'help',
-    minArgs: 0,
-    maxArgs: 0,
-    callback: (message, arguments, text) => {
+  commands: ['help', 'h'],
+  description: "Describes all of this bot's commands",
+  callback: (message, arguments, text) => {
+    let reply = 'I am the tutorial bot, here are my supported commands:\n\n'
 
-    const helpEmbed = new Discord.MessageEmbed()
+    const commands = loadCommands()
+
+    for (const command of commands) {
+      // Check for permissions
+      let permissions = command.permission
+
+      if (permissions) {
+        let hasPermission = true
+        if (typeof permissions === 'string') {
+          permissions = [permissions]
+        }
+
+        for (const permission of permissions) {
+          if (!message.member.hasPermission(permission)) {
+            hasPermission = false
+            break
+          }
+        }
+
+        if (!hasPermission) {
+          continue
+        }
+      }
+
+      // Format the text
+      const mainCommand =
+        typeof command.commands === 'string'
+          ? command.commands
+          : command.commands[0]
+      const args = command.expectedArgs ? ` ${command.expectedArgs}` : ''
+      const { description } = command
+
+      
+      const helpEmbed = new Discord.MessageEmbed()
 .setColor('#00FF00')
 .setTitle(`Help`)
 .setDescription(`This is the help command use -help to view it.
 
 List of commands:
 
--help, what you're viewing right now
-
--ping, shows ping to Tubb
-
--bal, to view your account balance
-
--pay, to pay others Strands
-
-More commands in development!`)
-
-message.reply(helpEmbed);
+**${prefix}${mainCommand}${args}** = ${description}\n`)
+    
     }
+
+    message.channel.send(helpEmbed)
+  },
 }
