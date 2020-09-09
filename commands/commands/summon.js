bb7ccad9f33d6bdb2586b39a66e78488d4e00b52
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-08 20:07:57 -0500

    Update summon.js

---
+++ commands/commands/summon.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const userId = mention.id
+
+module.exports = {
+    commands: 'summon',
+    minArgs: 0,
+    maxArgs: 0,
+    callback: (message, arguments, text) => {
+        const summonEmbed = new Discord.MessageEmbed()
+        .setColor('#9400D3')
+        .setTitle(`Ritual of Kys`)
+        .setDescription('@427630841045319701')
+
+    message.reply(summonEmbed)  
+    }
+}
