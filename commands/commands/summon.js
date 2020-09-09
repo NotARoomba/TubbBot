efcd66798552907d25309bf5477b6597accb12f5
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-08 20:25:37 -0500

    Update summon.js

--- commands/commands/summon.js
+++ commands/commands/summon.js
@@ -12,6 +12,14 @@ module.exports = {
         .setColor('#9400D3')
         .setTitle(`Ritual of Kys`)
         .setDescription(`<@${summoned}>`)
+        .setDescription(`<@${summoned}>`)
+        .setDescription(`<@${summoned}>`)
+        .setDescription(`<@${summoned}>`)
+        .setDescription(`<@${summoned}>`)
+        .setDescription(`<@${summoned}>`)
+        .setDescription(`<@${summoned}>`)
+        .setDescription(`<@${summoned}>`)
+        .setDescription(`<@${summoned}>`)
 
     message.reply(summonEmbed)  
     }
