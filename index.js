ef11e5e5e8c08480484e8e0485d2d782b48ded5f
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-12 10:39:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -41,29 +41,5 @@ client.on('ready', async () => {
   });
 
 
-
-
-//client.on('guildMemberAdd', member => {
-    //const channel = member.guild.channels.cache.find(channel => channel.name === "welcome");
-    //if(!channel) return;
-
-//const welcomeEmbed = new Discord.MessageEmbed()
-    //.setColor('#8B0000')
-    //.setTitle(`Welcome`)
-    //.setDescription(`Welcome to Corona, ${member}! Once you join, you will be infected with the corona virus. There are 3 stages, diagnosed, recovering, and recovered. Once recovered you will be free just to hang out on this server and play, but until then, you have a hard battle. After you recovered, you will be vaccinated which will make sure you're never infected ever again. The way you level up and get through the virus is simply by just hanging out and "spreading" the word about this server! Please read #rules and #roles. Goodluck and may the odds be ever in your favor.(Btw -help)`)
-
-//channel.send(welcomeEmbed);
-
-//});
-
-
-
-    
-
-
-
-
-
-
 //client.login('NzUwMTIzNjc3NzM5MTIyODE5.X019HQ.1_2Ti4y-h9PJljBHzotdA36p7vY')
 client.login(process.env.token);
