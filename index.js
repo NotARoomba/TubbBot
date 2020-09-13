9a1af914bd36e351efafc43f3607991de5b0b572
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-13 12:54:50 -0500

    ban

--- index.js
+++ index.js
@@ -39,22 +39,7 @@ client.on('ready', async () => {
 
   
 
-  command(client, 'kick', (message) => {
-    const { member, mentions } = message
 
-    const tag = `<@${member.id}>`
- 
-      const target = mentions.users.first()
-      if (target) {
-        const targetMember = message.guild.members.cache.get(target.id)
-        targetMember.kick()
-        message.channel.send(`${tag} That user has been kicked`)
-      } else {
-        message.channel.send(`${tag} Please specify someone to kick.`)
-      }
-     
-
-    })
   });
 
 
