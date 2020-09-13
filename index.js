f73298fa33d60d8141ee0a8f16d3826a6c2be411
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-12 19:22:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -37,53 +37,7 @@ client.on('ready', async () => {
   loadCommands(client)
   loadFeatures(client)
 
-  command(client, 'ban', (message) => {
-    const { member, mentions } = message
-
-    const tag = `<@${member.id}>`
-
-    if (
-      member.hasPermission('ADMINISTRATOR') ||
-      member.hasPermission('BAN_MEMBERS')
-    ) {
-      const target = mentions.users.first()
-      if (target) {
-        const targetMember = message.guild.members.cache.get(target.id)
-        targetMember.ban()
-        message.channel.send(`${tag} That user has been`)
-      } else {
-        message.channel.send(`${tag} Please specify someone to ban.`)
-      }
-    } else {
-      message.channel.send(
-        `${tag} You do not have permission to use this command.`
-      )
-    }
-  })
-
-  command(client, 'kick', (message) => {
-    const { member, mentions } = message
-
-    const tag = `<@${member.id}>`
-
-    if (
-      member.hasPermission('ADMINISTRATOR') ||
-      member.hasPermission('KICK_MEMBERS')
-    ) {
-      const target = mentions.users.first()
-      if (target) {
-        const targetMember = message.guild.members.cache.get(target.id)
-        targetMember.kick()
-        message.channel.send(`${tag} That user has kicked`)
-      } else {
-        message.channel.send(`${tag} Please specify someone to kick.`)
-      }
-    } else {
-      message.channel.send(
-        `${tag} You do not have permission to use this command.`
-      )
-    }
-  })
+  
 
   });
 
