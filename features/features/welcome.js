36991fc0e58c716ee0e1eee6a16aee0911a383d6
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 16:46:35 -0500

    Update welcome.js

--- features/features/welcome.js
+++ features/features/welcome.js
@@ -1,19 +1,92 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const mongo = require('@util/mongo')
+const command = require('@util/command')
+const welcomeSchema = require('@schemas/welcome-schema')
 
 module.exports = (client) => {
-    const channelId = '750766854360006676' // welcome channel
-    const rules = '750556623348564038'  // rules and info
-    const roles = '751559736096456885'
-    
-    client.on('guildMemberAdd', (member) => {
-          const welcomeEmbed = new Discord.MessageEmbed()
+  //!setwelcome <message>
+  const cache = {} // guildId: [channelId, text]
+
+  command(client, 'setwelcome', async (message) => {
+    const { member, channel, content, guild } = message
+
+    if (!member.hasPermission('ADMINISTRATOR')) {
+      channel.send('You do not have permission to run this command.')
+      return
+    }
+
+    let text = content
+
+    const split = text.split(' ')
+
+    if (split.length < 2) {
+      
+      const welcomeEmbed = new Discord.MessageEmbed()
             .setColor('#8B0000')
             .setTitle(`Welcome`)
             .setDescription(`Welcome to Corona, ${member}! Once you join, you will be infected with the corona virus. There are 3 stages, diagnosed, recovering, and recovered. Once recovered you will be free just to hang out on this server and play, but until then, you have a hard battle. After you recovered, you will be vaccinated which will make sure you're never infected ever again. The way you level up and get through the virus is simply by just hanging out and "spreading" the word about this server! Please read ${member.guild.channels.cache.get(rules).toString()} and ${member.guild.channels.cache.get(roles).toString()}. Goodluck and may the odds be ever in your favor.
             (Btw -help)`)
-    
-        const channel = member.guild.channels.cache.get(channelId)
-        channel.send(welcomeEmbed)
+      
+      channel.send(welcomeEmbed)
+      return
+    }
+
+    split.shift()
+    text = split.join(' ')
+
+    cache[guild.id] = [channel.id, text]
+
+    await mongo().then(async (mongoose) => {
+      try {
+        await welcomeSchema.findOneAndUpdate(
+          {
+            _id: guild.id,
+          },
+          {
+            _id: guild.id,
+            channelId: channel.id,
+            text,
+          },
+          {
+            upsert: true,
+          }
+        )
+      } finally {
+        mongoose.connection.close()
+      }
+    })
+  })
+
+  const onJoin = async (member) => {
+    const { guild } = member
+
+    let data = cache[guild.id]
+
+    if (!data) {
+      console.log('FETCHING FROM DATABASE')
+
+      await mongo().then(async (mongoose) => {
+        try {
+          const result = await welcomeSchema.findOne({ _id: guild.id })
+
+          cache[guild.id] = data = [result.channelId, result.text]
+        } finally {
+          mongoose.connection.close()
+        }
       })
+    }
+
+    const channelId = data[0]
+    const text = data[1]
+
+    const channel = guild.channels.cache.get(channelId)
+    channel.send(text.replace(/<@>/g, `<@${member.id}>`))
+  }
+
+  command(client, 'simjoin', (message) => {
+    onJoin(message.member)
+  })
+
+  client.on('guildMemberAdd', (member) => {
+    onJoin(member)
+  })
 }
