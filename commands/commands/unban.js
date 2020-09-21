908988d54985200d054b8d2db5f09e2e43f6b76b
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 16:06:07 -0500

    Update unban.js

--- commands/commands/unban.js
+++ commands/commands/unban.js
@@ -7,18 +7,27 @@ module.exports = {
     permissions: 'ADMINISTRATOR',
     callback: async (message, args) => {
 
-        let bannedMember = parseInt(args[0]);
-        if(isNaN(bannedMember)) return message.channel.send("You need to give me a valid ID!");
-
-    let reason = args.slice(1).join(' ');
-        if(!reason) return reason = "No reason given.";
-    
-        
-    try {
-        message.guild.members.unban(bannedMember, reason);
-        message.channel.send(`Succesfully unbanned ${bannedMember}!`)
-    } catch(e) {
-        console.log(e.message);
-    }
+        if (!isNaN(args[0])) {
+            const bannedMember = message.guild.members.cache.get(args[0]) // Get the `member` property instead to recall later.
+            var reason = args.slice(1).join(" ");
+            if(!reason) {
+              reason = "No reason given!"
+            }
+            if (bannedMember) {
+              bannedMember
+                message.guild.members.unban(bannedMember.id, reason)
+                .then(() => {
+                  message.channel.send(`Successfully unbanned **${bannedMember.user.tag}**`);
+                })
+                .catch(err => {
+                  message.channel.send('I was unable to unban the member');
+                  console.error(err);
+                });
+            } else {
+              message.channel.send("That user isn't in this guild!");
+            }
+          } else {
+            message.channel.send("You need to provide an user ID to unban");
+          }
     }
 }
