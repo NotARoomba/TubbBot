577697e89d99ebedf7bc35cdff2d63de92227625
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 12:12:32 -0500

    Update unban.js

--- commands/commands/unban.js
+++ commands/commands/unban.js
@@ -13,8 +13,13 @@ module.exports = {
         
         const target = mentions.users.first()
       if (target) {
-        const targetMember = message.guild.members.cache.get(target.id)
-        targetMember.guild.unban()
+        let userID = args[0]
+      msg.guild.fetchBans().then(bans=> {
+      if(bans.size == 0) return 
+      let bUser = bans.find(b => b.user.id == userID)
+      if(!bUser) return
+      msg.guild.members.unban(bUser.user)
+})
 
         const unbyesEmbed = new Discord.MessageEmbed()
     .setColor('#228B22')
