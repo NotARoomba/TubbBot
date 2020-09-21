e64c43f4aa0663ca734d8183c36652561652e59a
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 15:40:29 -0500

    Update unban.js

--- commands/commands/unban.js
+++ commands/commands/unban.js
@@ -6,28 +6,6 @@ module.exports = {
     commands: 'unban',
     permissions: 'ADMINISTRATOR',
     callback: async (message, args) => {
-        
-    
-
-    if(!args[0]) return message.channel.send("Give me a valid ID"); 
-
-    let bannedMember;
-    
-    try{                                                            
-        bannedMember = await bot.users.fetch(args[0])
-    }catch(e){
-        if(!bannedMember) return message.channel.send("That's not a valid ID")
-    }
-
-    try {
-            await message.guild.fetchBan(args[0])
-        } catch(e){
-            message.channel.send('This user is not banned.');
-            return;
-        }
-
-    let reason = args.slice(1).join(" ")
-    if(!reason) reason = "..."
 
     if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("I can't do that")
     message.delete()
