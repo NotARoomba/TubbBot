06db7a5433caf8b9ce6ce2a843367d8f2ea063b0
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 11:51:00 -0500

    Create ban.js

---
+++ commands/commands/ban.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+module.exports = {
+    commands: 'ban',
+    permissionError: 'You must be an administrator to use this command.',
+    permissions: 'ADMINISTRATOR',
+    callback: (message) => {
+        const { member, mentions, arguments} = message
+
+        const tag = `<@${member.id}>`
+        
+        const target = mentions.users.first()
+      if (target) {
+        const targetMember = message.guild.members.cache.get(target.id)
+        targetMember.ban()
+
+        const banyesEmbed = new Discord.MessageEmbed()
+    .setColor('#228B22')
+    .setTitle(`Success`)
+    .setDescription(`${tag} That user has been banned`)
+
+
+        message.channel.send(banyesEmbed)
+      } else {
+        
+        const banerrEmbed = new Discord.MessageEmbed()
+            .setColor('#FFFF00')
+            .setTitle(`Error`)
+            .setDescription(`${tag} Please specify someone to ban.`)
+        
+        message.channel.send(banerrEmbed)
+      }
+    }
+}
