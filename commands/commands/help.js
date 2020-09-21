4ff1251dae2126fae052afd8a745a80f66e2b1d0
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 18:12:24 -0500

    Update help.js

--- commands/commands/help.js
+++ commands/commands/help.js
@@ -1,11 +1,19 @@
 const loadCommands = require('@root/commands/load-commands')
 const { prefix } = require('@root/config.json')
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
 module.exports = {
   commands: ['help', 'h'],
   description: "Describes all of this bot's commands",
   callback: (message, arguments, text) => {
-    let reply = 'I am TUBB (The Utility (B*tch) Bot):\n\n'
+    
+    const helpEmbed = new Discord.MessageEmbed()
+    .setColor('#C0C0C0')
+    .setTitle(`Help`)
+    .setDescription('I am TUBB (The Utility (B*tch) Bot):\n\n')
+
+    let reply = helpEmbed
 
     const commands = loadCommands()
 
@@ -38,10 +46,14 @@ module.exports = {
           : command.commands[0]
       const args = command.expectedArgs ? ` ${command.expectedArgs}` : ''
       const { description } = command
-
-      reply += `**${prefix}${mainCommand}${args}** = ${description}\n`
+      
+      const help2Embed = new Discord.MessageEmbed()
+      .setColor('#C0C0C0')
+      .setTitle(`Help`)
+      .setDescription(`**${prefix}${mainCommand}${args}** = ${description}\n`)
+       
     }
 
-    message.channel.send(reply)
+    message.channel.send(reply += help2Embed) 
   },
 }
