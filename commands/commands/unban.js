17237e329316572349116731057cfbea6d1fb8e0
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 15:34:44 -0500

    Update unban.js

--- commands/commands/unban.js
+++ commands/commands/unban.js
@@ -5,38 +5,39 @@ const client = new Discord.Client()
 module.exports = {
     commands: 'unban',
     permissions: 'ADMINISTRATOR',
-    callback: async (message, args) => {
+    callback: async (client, message, args) => {
         
-        
+    if(!message.member.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("You can't do that.")
 
-        if(!args[0]) return message.channel.send("Give me a valid ID"); 
-       
-    
-        let bannedMember;
-        
-        try{                                                            
-            bannedMember = await bot.users.fetch(args[0])
-        }catch(e){
-            if(!bannedMember) return message.channel.send("That's not a valid ID")
+    if(!args[0]) return message.channel.send("Give me a valid ID"); 
+    //This if() checks if we typed anything after "!unban"
+
+    let bannedMember;
+    //This try...catch solves the problem with the await
+    try{                                                            
+        bannedMember = await bot.users.fetch(args[0])
+    }catch(e){
+        if(!bannedMember) return message.channel.send("That's not a valid ID")
+    }
+
+    //Check if the user is not banned
+    try {
+            await message.guild.fetchBan(args[0])
+        } catch(e){
+            message.channel.send('This user is not banned.');
+            return;
         }
-    
-        
-        try {
-                await message.guild.fetchBan(args[0])
-            } catch(e){
-                message.channel.send('This user is not banned.');
-                return;
-            }
-    
-        let reason = args.slice(1).join(" ")
-        if(!reason) reason = "..."
-    
-        
-        try {
-            message.guild.members.unban(bannedMember, {reason: reason})
-            message.channel.send(`${bannedMember.tag} was readmitted.`)
-        } catch(e) {
-            console.log(e.message)
+
+    let reason = args.slice(1).join(" ")
+    if(!reason) reason = "..."
+
+    if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("I can't do that")
+    message.delete()
+    try {
+        message.guild.members.unban(bannedMember, {reason: reason})
+        message.channel.send(`${bannedMember.tag} was readmitted.`)
+    } catch(e) {
+        console.log(e.message)
     }
-    }
+}
 }
