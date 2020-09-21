66e353b3d4b7853498785f54b21560e6f9d0557f
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 13:51:57 -0500

    Update unban.js

--- commands/commands/unban.js
+++ commands/commands/unban.js
@@ -9,16 +9,16 @@ module.exports = {
 	    const member = args[0];
 
         if (!member) {
-             return message.reply(`Please enter a id!`)
+             return message.channel.send(`Please enter a id!`)
         }
 
         try {
             message.guild.fetchBans().then(bans => {
                 message.guild.members.unban(member)
             })
-            await message.reply(`${member} has been unbanned!`)
+            await message.channel.re(`${member} has been unbanned!`)
         } catch (e) {
-            return message.reply(`An error occured!`)
+            return message.channel.send(`An error occured!`)
         }
     }
 }
