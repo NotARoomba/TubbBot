60bd8707282c5f1460b7e5d4f4a1f19d23373e97
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 16:20:02 -0500

    Update unban.js

--- commands/commands/unban.js
+++ commands/commands/unban.js
@@ -13,13 +13,13 @@ module.exports = {
    {
         
        
-        if(!args[1]) return  message.channel.send('**Mention User Or Type ID**');
-        if(args[1].length < 16) return message.reply('** This ID is not id user!**');
+        if(!args[1]) return  message.channel.send('Mention User Or Type ID');
+        if(args[1].length < 16) return message.reply('This ID is not id user!');
         message.guild.fetchBans().then(bans => {
             var Found = bans.find(m => m.id === args[1]);
-            if(!Found) return message.channel.send(`**I Can't Find <@${args[1]}> In The Ban List**`);
+            if(!Found) return message.channel.send(`I Can't Find <@${args[1]}> In The Ban List`);
             message.guild.unban(args[1]);
-            message.channel.send(`**<@${args[1]}> Unbanned!**`);
+            message.channel.send(`<@${args[1]}> Unbanned!`);
             }
 
         )}
