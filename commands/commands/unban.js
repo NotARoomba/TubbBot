4484144823d25862f24d81f400c6a89499fd7a4f
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 15:18:51 -0500

    Update unban.js

--- commands/commands/unban.js
+++ commands/commands/unban.js
@@ -7,16 +7,20 @@ module.exports = {
     permissions: 'ADMINISTRATOR',
     callback: async (message, args) => {
         
-        if(isNaN(args[0])) return message.channel.send("You need to provide an ID.")
-        let bannedMember =  client.users.fetch(args[0])
-            if(!bannedMember) return message.channel.send("Please provide a user id to unban someone!")
-    
-    
-        try {
-            message.guild.members.unban(bannedMember, reason)
-            message.channel.send(`**${bannedMember.tag}** has been unbanned from the guild!`)
-        } catch(e) {
-            console.log(e.message)
-        }
+           if(isNaN(args[0])) return message.channel.send("You need to provide an ID.")
+    let bannedMember =  client.users.fetch(args[0])
+        if(!bannedMember) return message.channel.send("Please provide a user id to unban someone!")
+
+    let reason = args.slice(1).join(" ")
+        if(!reason) reason = "No reason given!"
+
+    if(!message.guild.me.hasPermission(["BAN_MEMBERS"])) return message.channel.send("I dont have permission to perform this command!")|
+    message.delete()
+    try {
+        message.guild.members.unban(bannedMember, reason)
+        message.channel.send(`**${bannedMember.tag}** has been unbanned from the guild!`)
+    } catch(e) {
+        console.log(e.message)
+    }
     }
 }
