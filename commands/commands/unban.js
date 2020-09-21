9ee57d4ca8c9d0bada672685cbbdde3ae480ff4e
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 14:40:20 -0500

    Update unban.js

--- commands/commands/unban.js
+++ commands/commands/unban.js
@@ -7,7 +7,7 @@ module.exports = {
     permissions: 'ADMINISTRATOR',
     callback: async (client, message, args) => {
         
-        const member = message.mentions.member.first();
+        const member = message.mentions.members.first();
     
         if(!args[0]) return message.channel.send('Please specify a user');
 
