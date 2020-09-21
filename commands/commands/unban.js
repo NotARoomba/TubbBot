3a65e7d607ec4fdf64636097cdbc47ed5114c2e4
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 12:35:38 -0500

    Update unban.js

--- commands/commands/unban.js
+++ commands/commands/unban.js
@@ -8,25 +8,22 @@ module.exports = {
     permissions: 'ADMINISTRATOR',
     callback: async (message) => {
         
-        let User = args[0];
-        let Reason = args.slice(1).join(` `);
-        if (!User) return message.reply(`Who are we unbanning?`);
-        if (!Reason) Reason = `Unbanned by ${message.author.tag}`
-        
-        message.guild.fetchBans()
-        .then(bans => {
-        if (bans.some(u => User.includes(u.username))) {
-        let user = bans.find(user => user.username === User);
-        
-        message.guild.unban(user.id, Reason);
+        if(!bot.hasPermission(message, "BAN_MEMBERS")) return message.channel.send("You do not have permission to do this.");
+	
+        let search = args.join(" ");
+        if(!search) return message.channel.send("Please provide a valid ID or name.");
+    
+        try {
+            let bans = await message.guild.fetchBans();
+            let banned = bans.get(search) || bans.find(u => u.tag.toLowerCase().includes(search.toLowerCase()));
+            
+            if(!banned) return message.channel.send("I could not find a banned user by this ID or name.");
+    
+            await message.guild.unban(banned);
+    
+            message.channel.send(`${banned.tag} has been unbanned.`);
+        } catch(e) {
+            message.channel.send(`Unban failed: ${reason}`)
         }
-        else if (bans.some(u => User.includes(u.id))) {
-        
-        message.guild.unban(User, Reason);
-        }
-        else {
-        return message.reply(`This person is not banned`);
-        }
-        });
     }
 }
