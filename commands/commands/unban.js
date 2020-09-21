536a9324feeeda211f2965a55043d2c21d6707ca
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 12:02:33 -0500

    Update unban.js

--- commands/commands/unban.js
+++ commands/commands/unban.js
@@ -5,15 +5,19 @@ module.exports = {
     commands: 'unban',
     permissionError: 'You must be an administrator to use this command.',
     permissions: 'ADMINISTRATOR',
-    callback: (message) => {
+    callback: async (message) => {
         const { member, mentions, arguments} = message
 
         const tag = `<@${member.id}>`
+        let unbanned = message.mentions.users.first() || client.users.resolve(args[0]);
+        let reason = args.slice(1).join(" ");
+        let member = await client.users.fetch(unbanned);
+        let ban = await message.guild.fetchBans();
         
         const target = mentions.users.first()
       if (target) {
-        const targetMember = message.guild.members.cache.get(target.id)
-        targetMember.guild.unban()
+        var user = ban.get(member.id);
+    message.guild.members.unban(member);
 
         const unbyesEmbed = new Discord.MessageEmbed()
     .setColor('#228B22')
