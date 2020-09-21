397c2d69a61adb538cadddfa9494179bbe873d71
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 16:30:13 -0500

    Update unban.js

--- commands/commands/unban.js
+++ commands/commands/unban.js
@@ -5,23 +5,22 @@ const client = new Discord.Client()
 module.exports = {
     commands: 'unban',
     permissions: 'ADMINISTRATOR',
-    callback: (message, args) => {
+    callback: async (client, message, args) => {
 
-        var command = message.content.toLowerCase().split(" ")[0];
-    var args = message.content.toLowerCase().split(" ");
-    var user = message.mentions.users.first()
-   {
-        
-       
-        if(!args[1]) return  message.channel.send('Mention User Or Type ID');
-        if(args[1].length < 16) return message.reply('This ID is not id user!');
-        message.guild.fetchBans().then(bans => {
-            var Found = bans.find(m => m.id === args[1]);
-            if(!Found) return message.channel.send(`I Can't Find <@${args[1]}> In The Ban List`);
-            message.guild.unban(args[1]);
-            message.channel.send(`<@${args[1]}> Unbanned!`);
-            }
+        const member = args[0];
 
-        )}
+        if (!member) {
+             return message.channel.send(`Please enter a id!`)
         }
+
+        try {
+            message.guild.fetchBans().then(bans => {
+                message.guild.members.unban(member)
+            })
+            await message.channel.send(`${member} has been unbanned!`)
+        } catch (e) {
+            return message.channel.send(`An error occured!`)
+        }
+
     }
+}
