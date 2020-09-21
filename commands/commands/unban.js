3fb8739976440bf2020d89fd62f9440444d2a8dd
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 15:48:58 -0500

    Update unban.js

--- commands/commands/unban.js
+++ commands/commands/unban.js
@@ -7,35 +7,18 @@ module.exports = {
     permissions: 'ADMINISTRATOR',
     callback: async (message, args) => {
 
-        if(!args[0]) return message.channel.send("Give me a valid ID"); 
-        //This if() checks if we typed anything after "!unban"
+        let bannedMember = parseInt(args[0]);
+        if(isNaN(bannedMember)) return message.channel.send("You need to give me a valid ID!");
+
+    let reason = args.slice(1).join(' ');
+        if(!reason) return reason = "No reason given.";
     
-        let bannedMember;
-        //This try...catch solves the problem with the await
-        try{                                                            
-            bannedMember = await client.users.fetch(args[0])
-        }catch(e){
-            if(!bannedMember) return message.channel.send("That's not a valid ID")
-        }
-    
-        //Check if the user is not banned
-        try {
-                await message.guild.fetchBan(args[0])
-            } catch(e){
-                message.channel.send('This user is not banned.');
-                return;
-            }
-    
-        let reason = args.slice(1).join(" ")
-        if(!reason) reason = "..."
-    
-        if(!message.guild.me.hasPermission(["BAN_MEMBERS", "ADMINISTRATOR"])) return message.channel.send("I can't do that")
-        message.delete()
-        try {
-            message.guild.members.unban(bannedMember, {reason: reason})
-            message.channel.send(`${bannedMember.tag} was readmitted.`)
-        } catch(e) {
-            console.log(e.message)
-}
+    message.delete()
+    try {
+        message.guild.members.unban(bannedMember, reason);
+        message.channel.send(`Succesfully unbanned ${bannedMember}!`)
+    } catch(e) {
+        console.log(e.message);
+    }
     }
 }
