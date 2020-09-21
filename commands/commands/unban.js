796dff781f9ea1664f771aeddcb83a500426801e
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 15:27:09 -0500

    Update unban.js

--- commands/commands/unban.js
+++ commands/commands/unban.js
@@ -5,24 +5,21 @@ const client = new Discord.Client()
 module.exports = {
     commands: 'unban',
     permissions: 'ADMINISTRATOR',
-    callback: async ( message, args) => {
+    callback: async (message, args) => {
         
-           if(isNaN(args[0])) return message.channel.send("You need to provide an ID.")
-    let bannedMember =  client.users.fetch(args[0])
-        if(!bannedMember) return message.channel.send("Please provide a user id to unban someone!")
+            const member = args[0];
 
-    let reason = args.slice(1).join(" ")
-        if(!reason) reason = "No reason given!"
-        
-        if (!message.guild.members.get(bannedMember)) return message.reply("This user is not banned!")
-
-    if(!message.guild.me.hasPermission(["BAN_MEMBERS"])) return message.channel.send("I dont have permission to perform this command!")|
-    message.delete()
-    try {
-        message.guild.members.unban(bannedMember, reason)
-        message.channel.send(`**${bannedMember.tag}** has been unbanned from the guild!`)
-    } catch(e) {
-        console.log(e.message)
-    }
+           if (!member) {
+                return message.channel.send(`Please enter a id!`)
+           }
+   
+           try {
+               message.guild.fetchBans().then(bans => {
+                   message.guild.members.unban(member)
+               })
+               await message.channel.send(`${member} has been unbanned!`)
+           } catch (e) {
+               return message.channel.send(`An error occured!`)
+           }
     }
 }
