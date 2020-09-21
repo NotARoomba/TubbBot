88ddcbb42d46f90bd5152475ee0a95d841108ae4
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 16:32:04 -0500

    Update unban.js

--- commands/commands/unban.js
+++ commands/commands/unban.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js')
-const client = new Discord.Client()
+
  
 
 module.exports = {
     commands: 'unban',
     permissions: 'ADMINISTRATOR',
-    callback: async (client, message, args) => {
+    callback: async (message, args) => {
 
         const member = args[0];
 
