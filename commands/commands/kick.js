2d9f9dee45b06fbd711779437d1f62c7046c0431
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 11:08:02 -0500

    Create kick.js

---
+++ commands/commands/kick.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+module.exports = {
+    commands: 'kick',
+    minArgs: 0,
+    maxArgs: 0,
+    callback: (message) => {
+        const { member, mentions } = message
+
+        const tag = `<@${member.id}>`
+        
+        const target = mentions.users.first()
+      if (target) {
+        const targetMember = message.guild.members.cache.get(target.id)
+        targetMember.kick()
+        message.channel.send(`${tag} That user has kicked`)
+      } else {
+        message.channel.send(`${tag} Please specify someone to kick.`)
+      }
+    }
+}
