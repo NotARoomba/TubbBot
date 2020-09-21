ad8d99cf0d731e7f494c6191768fe5e6d215fb14
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-21 13:43:41 -0500

    Update unban.js

--- commands/commands/unban.js
+++ commands/commands/unban.js
@@ -6,16 +6,21 @@ module.exports = {
     commands: 'unban',
     permissionError: 'You must be an administrator to use this command.',
     permissions: 'ADMINISTRATOR',
-    callback: async (message, args) => {
+    callback: async (client, message, args) => {
 	
-        let userID = args[0]
-        msg.guild.fetchBans().then(bans=> {
-        if(bans.size == 0) return 
-        let bUser = bans.find(b => b.user.id == userID)
-        message.channel.send(`Error`)
-        if(!bUser) return
-        msg.guild.members.unban(bUser.user)
-        message.channel.send(`Unbanned!`)
-    })
+        const member = args[0];
+
+        if (!member) {
+             return message.channel.send(`Please enter a id!`)
+        }
+
+        try {
+            message.guild.fetchBans().then(bans => {
+                message.guild.members.unban(member)
+            })
+            await message.channel.send(`${member} has been unbanned!`)
+        } catch (e) {
+            return message.channel.send(`An error occured!`)
+        }
     }
 }
