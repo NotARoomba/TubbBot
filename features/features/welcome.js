1076691c7b1890a3034e4c251cfd85b89adbc971
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-22 07:48:36 -0500

    Update welcome.js

--- features/features/welcome.js
+++ features/features/welcome.js
@@ -1,29 +1,18 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const Canvacord = require("canvacord");
-client.canvas = new Canvacord();
 
 module.exports = (client) => {
-    // welcome channel
+  const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'welcome', 'welcome-channel')  // welcome channel
     const rules = '757769773148012655'  // rules and info
     //const roles = '751559736096456885'
     
     client.on('guildMemberAdd', (member) => {
-      const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'welcome', 'welcome-channel')
-          
-  const canvas = Canvas.createCanvas(700, 250);
-	const ctx = canvas.getContext('2d');
-
-	const background = await Canvas.loadImage('@root/wallpaper.jpg');
-	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-	ctx.strokeStyle = '#74037b';
-	ctx.strokeRect(0, 0, canvas.width, canvas.height);
-
-	
-
-	channel.send(`Welcome to the Server, ${member}! Please read ${member.guild.channels.cache.get(rules).toString()}. Thank you for joining this Server and we hope you have a good time!`, attachment);
-});
+          const welcomeEmbed = new Discord.MessageEmbed()
+            .setColor('#8B0000')
+            .setTitle(`Welcome`)
+            .setDescription(`Welcome to this Server, ${member}! Please read ${member.guild.channels.cache.get(rules).toString()}. Thank you for joining this server and we hope you have a good time!
+            (Btw -help)`)
   
-      
+        welcomChannel.send(welcomeEmbed)
+      })
 }
