f38e6bd9861426d9ab3b1a2e30543cbe78b3ce6c
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-22 11:37:11 -0500

    gcjnf

--- commands/commands/rank.js
+++ commands/commands/rank.js
@@ -11,7 +11,7 @@ module.exports = {
 
         const canvas = Canvas.createCanvas(700, 250);
         const ctx = canvas.getContext('2d');
-        const background = await Canvas.loadImage('../../wallpaper.jpg');
+        const background = await Canvas.loadImage('@commands/wallpaper.jpg');
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
         const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
     }
