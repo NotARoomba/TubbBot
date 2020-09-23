f9caa9b48738c8d469c43699ff91348f2409ce74
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-23 13:44:38 -0500

    Update welcome.js

--- features/features/welcome.js
+++ features/features/welcome.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 module.exports = (member, message) => {
-  const channel = client.channels.find("757771111374258176")
     const rules = '757769773148012655'  // rules and info
     //const roles = '751559736096456885'
     
@@ -13,6 +12,6 @@ module.exports = (member, message) => {
             .setDescription(`Welcome to this Server, ${member}! Please read #rules . Thank you for joining this server and we hope you have a good time!
             (Btw -help)`)
   
-        channel.send(welcomeEmbed)
+            member.guild.channels.cache.get(`720631185688100867`).send(welcomeEmbed)
       })
 }
