51ec635174bce218082124a58ea82e0cc09c6ee0
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-23 11:25:56 -0500

    Update mongo.js

--- util/mongo.js
+++ util/mongo.js
@@ -4,6 +4,7 @@ module.exports = async () => {
   await mongoose.connect('mongodb+srv://L061571C5:89euzXX8IylP1DYn@tubbbot.kfqqn.mongodb.net/data?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   })
   return mongoose
 }
