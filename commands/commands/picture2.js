bc530af7b36eaae5520cd573afcf2f097489db0d
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-23 17:36:16 -0500

    Update picture2.js

--- commands/commands/picture2.js
+++ commands/commands/picture2.js
@@ -11,9 +11,12 @@ module.exports = {
         const ctx = canvas.getContext('2d');
         const background = await Canvas.loadImage('https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v268batch2-kul-02_2.jpg?bg=transparent&con=3&cs=srgb&dpr=1&fm=jpg&ixlib=php-3.1.0&q=80&usm=15&vib=3&w=1300&s=a5cfc956068e95f97f6df92d9d96439c')
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-        ctx.font = '60px sans-serif';
-        ctx.fillStyle = '#000000';
-        ctx.fillText('test', canvas.width / 2.5, canvas.height / 1.8);
+        ctx.beginPath();
+        ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	    ctx.closePath();
+	    ctx.clip();
+        const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+	    ctx.drawImage(avatar, canvas.width / 2.5, canvas.height / 1.8);
         const welcome = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
         message.channel.send(welcome);
     }
