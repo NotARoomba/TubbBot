56d1d17a7849abaa331286c939e65bc4dcec7029
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-23 16:56:43 -0500

    Update picture.js

--- commands/commands/picture.js
+++ commands/commands/picture.js
@@ -11,21 +11,17 @@ module.exports = {
 
         let font = await jimp.loadFont(jimp.FONT_SANS_128_BLACK) 
         let welcome = await jimp.read('https://cdn.discordapp.com/attachments/757768055479861278/758090856325709885/bh.jpeg').then(font => {
-    font,
-    508,
-    200,
-    {
-      text: 'Hello world!',
-      alignmentX: jimp.HORIZONTAL_ALIGN_CENTER,
-      alignmentY: jimp.VERTICAL_ALIGN_MIDDLE
-    }
-   })
-         
+            jimp.read(member.user.displayAvatarURL).then(avatar => { //We take the user's avatar and declare it
+            avatar.resize(128, 128) //We resize the avatar 
+            welcome.composite(avatar, 20, 20) //We put the avatar on the image on the position 20, 20
+    });
         welcome.write('Welcome2.png')
         
     message.channel.send(``, { files: ["Welcome2.png"] })
       
 
-    }
+}
+)}
 
-};
+
+}
