cd9175648df979381d5b161c6eb708eaccd6eb01
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-23 15:36:30 -0500

    Update welcome.js

--- features/features/welcome.js
+++ features/features/welcome.js
@@ -2,17 +2,16 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 module.exports = client => {
-  const channel = member.guild.channels.cache.find(channel => channel.name === 'welcome')  // welcome channel
     const rules = '757769773148012655'  // rules and info
     //const roles = '751559736096456885'
     
-    client.on('guildMemberAdd', member => {
+    client.on('guildMemberAdd', (member) => {
           const welcomeEmbed = new Discord.MessageEmbed()
             .setColor('#8B0000')
             .setTitle(`Welcome`)
             .setDescription(`Welcome to this Server, ${member}! Please read ${member.guild.channels.cache.get(rules).toString()}. Thank you for joining this server and we hope you have a good time!
             (Btw -help)`)
-  
+        const channel = member.guild.channels.cache.find(channel => channel.name === 'welcome')
         channel.send(welcomeEmbed)
       })
 }
