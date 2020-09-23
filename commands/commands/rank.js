72601d44956340027ac9605c5d4ab9098dcd8fc0
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-23 08:06:31 -0500

    Update rank.js

--- commands/commands/rank.js
+++ commands/commands/rank.js
@@ -13,11 +13,8 @@ module.exports = {
         let welcome = await jimp.read('https://cdn.discordapp.com/attachments/757768055479861278/758090856325709885/bh.jpeg')
         welcome.print(font, 508, 200, `Hello, ${message.author.tag}`) 
         welcome.write('Welcome2.png')
-        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-       
-
-        message.channel.send(``, { files: ["Welcome2.png"] })
+        
+    message.channel.send(``, { files: ["Welcome2.png"] })
       
 
     }
