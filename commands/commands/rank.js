c9620d05130842b0363bd390d1f2ff41f59c7eb7
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-23 09:38:59 -0500

    Update rank.js

---
+++ commands/commands/rank.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const xp = require('@features/xp')
+const client = new Discord.Client()
+
+
+module.exports = {
+    commands: 'rank',
+    minArgs: 0,
+    maxArgs: 0,
+    description: 'I wanna be, the very best...',
+    callback: (message, arguments, text) => {
+        const target = message.mentions.users.first() || message.author
+        const targetId = target.id
+    
+        const guildId = message.guild.id
+        const userId = target.id
+    
+        const xp = await xp.getNeededXP(guildId, userId)
+    
+        const balEmbed = new Discord.MessageEmbed()
+            .setColor('#000080')
+            .setTitle(`Balance`)
+            .setDescription(`Your rank ${level} with ${xp} experience! You now need ${getNeededXP(
+                level
+              )} XP to level up again.`)
+          message.reply(balEmbed)
+      },
+}
+
