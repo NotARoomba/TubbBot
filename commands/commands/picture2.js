094ef7c78ac017cca59030465eef80758e562e06
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-24 11:08:00 -0500

    Update picture2.js

--- commands/commands/picture2.js
+++ commands/commands/picture2.js
@@ -12,7 +12,6 @@ module.exports = {
         const ctx = canvas.getContext('2d');
         const background = await Canvas.loadImage('https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v268batch2-kul-02_2.jpg?bg=transparent&con=3&cs=srgb&dpr=1&fm=jpg&ixlib=php-3.1.0&q=80&usm=15&vib=3&w=1300&s=a5cfc956068e95f97f6df92d9d96439c')
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-        
         ctx.beginPath();
         // Start the arc to form a circle
         ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
