17e594be9996fd7a40fc6af72a207ff7b864a4f2
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-23 21:26:27 -0500

    Update picture.js

--- commands/commands/picture.js
+++ commands/commands/picture.js
@@ -2,19 +2,16 @@ const client = new Discord.Client();
 const Discord = require('discord.js');
 var jimp = require('jimp');
 
-
-
-
 module.exports = {
     commands: ['picture', 'pic'],
     minArgs: 0,
     maxArgs: 0,
     description: '*click click*',
     callback: (message, arguments, text) => {
-
-        let font = jimp.loadFont(jimp.FONT_SANS_128_BLACK) 
-        let welcome = jimp.read('https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v268batch2-kul-02_2.jpg?bg=transparent&con=3&cs=srgb&dpr=1&fm=jpg&ixlib=php-3.1.0&q=80&usm=15&vib=3&w=1300&s=a5cfc956068e95f97f6df92d9d96439c').then(font => {
-        welcome.write(`welcome.png`);
+        async function resize() {
+        let welcome = await jimp.read('https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v268batch2-kul-02_2.jpg?bg=transparent&con=3&cs=srgb&dpr=1&fm=jpg&ixlib=php-3.1.0&q=80&usm=15&vib=3&w=1300&s=a5cfc956068e95f97f6df92d9d96439c').then(font => {
+        await image.resize(700, 300);
+        await image.writeAsync(`welcome.png`);
         
     message.channel.send(``, { files: ["welcome.png"] })
       
@@ -22,5 +19,5 @@ module.exports = {
 }
 )}
 
-
+}
 }
