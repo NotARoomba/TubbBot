9f5fecf74620c558c586e35b65209a95c91382e6
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-23 20:00:24 -0500

    editws

--- commands/commands/picture.js
+++ commands/commands/picture.js
@@ -10,7 +10,7 @@ module.exports = {
     callback: async (message) => {
 
         let font = await jimp.loadFont(jimp.FONT_SANS_128_BLACK) 
-        let welcome = await jimp.read('https://cdn.discordapp.com/attachments/757768055479861278/758090856325709885/bh.jpeg').then(font => {
+        let welcome = await jimp.read('https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v268batch2-kul-02_2.jpg?bg=transparent&con=3&cs=srgb&dpr=1&fm=jpg&ixlib=php-3.1.0&q=80&usm=15&vib=3&w=1300&s=a5cfc956068e95f97f6df92d9d96439c').then(font => {
         welcome.print(font, 10, 10, 'Hello world!');
         await welcome.writeAsync(`welcome.png`);
         
