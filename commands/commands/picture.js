d1357fd239bf5eef026e2553e9e4cc0468bd46e9
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-09-23 21:30:16 -0500

    Update picture.js

--- commands/commands/picture.js
+++ commands/commands/picture.js
@@ -7,7 +7,7 @@ module.exports = {
     minArgs: 0,
     maxArgs: 0,
     description: '*click click*',
-    callback: (message, arguments, text) => {
+    callback: async (message, arguments, text) => {
         async function resize() {
         let welcome = await jimp.read('https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v268batch2-kul-02_2.jpg?bg=transparent&con=3&cs=srgb&dpr=1&fm=jpg&ixlib=php-3.1.0&q=80&usm=15&vib=3&w=1300&s=a5cfc956068e95f97f6df92d9d96439c').then(font => {
         await image.resize(700, 300);
