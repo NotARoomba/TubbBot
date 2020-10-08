fa43217fff194ab36f231281c7d7a0b0cf008a1b
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-10-07 19:42:17 -0500

    stuff

--- commands/commands/botinfo.js
+++ commands/commands/botinfo.js
@@ -14,6 +14,7 @@ module.exports = {
     description: 'Sysinfo.exe',
     callback: async (message, arguments, text) => {
           
+
         
 
     const memberCount = guild.members.cache.filter(member => !member.user.bot).size;
