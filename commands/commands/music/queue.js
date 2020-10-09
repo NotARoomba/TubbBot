b22a46224fac70bcc1c0239c335b6b72f22bbcd7
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-10-08 20:03:16 -0500

    Update queue.js

--- commands/commands/music/queue.js
+++ commands/commands/music/queue.js
@@ -11,7 +11,7 @@ module.exports = {
     const description = queue.songs.map((song, index) => `${index + 1}. ${escapeMarkdown(song.title)}`);
 
     let queueEmbed = new MessageEmbed()
-      .setTitle("EvoBot Music Queue")
+      .setTitle("Tubb Music Queue")
       .setDescription(description)
       .setColor("#F8AA2A");
 
