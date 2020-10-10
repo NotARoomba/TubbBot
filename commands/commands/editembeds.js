b2b1b37109796914a3a15d5b11b4183234f1c2cf
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-10-09 20:54:48 -0500

    node modules

---
+++ commands/commands/editembeds.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+
+module.exports = {
+    commands: ['test'],
+    description: "Describes all of this bot's commands",
+    callback: (message, arguments, text) => {
+
+        const tstEmbed = new Discord.MessageEmbed()
+	.setTitle('Some title')
+	.setDescription('Description after the edit');
+
+message.edit(tstEmbed);
+    }
+}
