d173ec7244fc78e009a9d9ab00f2290acd51e6b7
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-11-01 14:40:10 -0600

    Update welcome.js

--- features/features/welcome.js
+++ features/features/welcome.js
@@ -72,7 +72,7 @@ module.exports = (client) => {
     const text = data[1]
 
     const channel = guild.channels.cache.get(channelId)
-    channel.send(text.replace[/<@>/g, `<@${member.id}>`, /<Server>/g, `<${message.guild.name}>`])
+    channel.send(text.replace(/<@>/g, `<@${member.id}>`))
   }
 
   command(client, 'simjoin', (message) => {
