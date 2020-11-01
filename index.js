90594b9d4997172013435826be086c570e505f6e
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-11-01 16:14:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.on('ready',  async () => {
   console.log('Tubb is online!')
   let activities = [ `${client.guilds.cache.size} servers` ], i = 0;
 
-  setInterval(() => client.user.setActivity(`${prefix}help | In ${activities[i ++ % activities.length]}`, { type: "LISTENING"},`${process.env.INTERVAL}`, 10000))
+  setInterval(() => client.user.setActivity(`${prefix}help | In ${activities[i ++ % activities.length]}`, { type: "LISTENING"},`${process.env.INTERVAL}`, 600000))
 
   
 
