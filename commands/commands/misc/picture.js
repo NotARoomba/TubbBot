5172d801786dd0caf4bae7358268ff12023001cc
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-11-01 18:10:21 -0600

    Update picture.js

--- commands/commands/misc/picture.js
+++ commands/commands/misc/picture.js
@@ -33,6 +33,7 @@ module.exports = {
         const canvas = Canvas.createCanvas(700, 300);
         const ctx = canvas.getContext('2d');
         const background = await Canvas.loadImage('https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v268batch2-kul-02_2.jpg?bg=transparent&con=3&cs=srgb&dpr=1&fm=jpg&ixlib=php-3.1.0&q=80&usm=15&vib=3&w=1300&s=a5cfc956068e95f97f6df92d9d96439c')
+        //const background = await Canvas.loadImage('https://www.fg-a.com/wallpapers/2020-black-crystalline-peaks-image.jpg')
         //const background = await Canvas.loadImage('')
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
         
@@ -46,14 +47,14 @@ module.exports = {
 	    ctx.fillText(`${message.member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
         ctx.beginPath();
         // Start the arc to form a circle
-        ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+        ctx.arc(350, 135, 100, 0, Math.PI * 2, true);
         // Put the pen down
         ctx.closePath();
         // Clip off the region you drew on
         ctx.clip();
 	const avatar = await Canvas.loadImage(message.author.displayAvatarURL({ format: 'jpg', size: 512}));
 	// Draw a shape onto the main canvas
-	ctx.drawImage(avatar, 350, 25, 200, 200);
+	ctx.drawImage(avatar, 250, 35, 200, 200);
 
         const picture = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
         message.channel.send(picture);
