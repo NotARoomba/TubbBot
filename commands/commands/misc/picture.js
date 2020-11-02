237b4c07c87015579f10805de32ed3720e2bebf9
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-11-01 18:37:44 -0600

    pic

--- commands/commands/misc/picture.js
+++ commands/commands/misc/picture.js
@@ -9,27 +9,6 @@ module.exports = {
     description: '*click click*',
     callback: async (message) => {
         
-        
-        const applyText = (canvas, text) => {
-            const ctx = canvas.getContext('2d');
-        
-            // Declare a base size of the font
-            let fontSize = 60;
-        
-            do {
-                // Assign the font to the context and decrement it so it can be measured again
-                ctx.font = `${fontSize -= 10}px sans-serif`;
-                // Compare pixel width of the text to the canvas minus the approximate avatar size
-            } while (ctx.measureText(text).width > canvas.width - 300);
-        
-            // Return the result to use in the actual canvas
-            return ctx.font;
-        };
-        
-        
-        
-        
-        
         const canvas = Canvas.createCanvas(700, 300);
         const ctx = canvas.getContext('2d');
         const background = await Canvas.loadImage('https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v268batch2-kul-02_2.jpg?bg=transparent&con=3&cs=srgb&dpr=1&fm=jpg&ixlib=php-3.1.0&q=80&usm=15&vib=3&w=1300&s=a5cfc956068e95f97f6df92d9d96439c')
@@ -39,22 +18,22 @@ module.exports = {
         
         ctx.strokeStyle = '#74037b';
 	    ctx.strokeRect(0, 0, canvas.width, canvas.height);
-        ctx.font = '32px sans-serif';
+        ctx.font = '30px sans-serif';
 	    ctx.fillStyle = '#00000';
-	    ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
-        ctx.font = '60px sans-serif';
+	    ctx.fillText('Welcome to the server,', 200, 225);
+        ctx.font = '25px sans-serif';
 	    ctx.fillStyle = '#00000';
-	    ctx.fillText(`${message.member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+	    ctx.fillText(`${message.member.username}!`, 300, 250);
         ctx.beginPath();
         // Start the arc to form a circle
-        ctx.arc(350, 135, 100, 0, Math.PI * 2, true);
+        ctx.arc(350, 110, 75, 0, Math.PI * 2, true);
         // Put the pen down
         ctx.closePath();
         // Clip off the region you drew on
         ctx.clip();
 	const avatar = await Canvas.loadImage(message.author.displayAvatarURL({ format: 'jpg', size: 512}));
 	// Draw a shape onto the main canvas
-	ctx.drawImage(avatar, 250, 35, 200, 200);
+	ctx.drawImage(avatar, 275, 35, 150, 150);
 
         const picture = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
         message.channel.send(picture);
