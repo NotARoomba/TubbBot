03cc70b9f27ea6e01ca3b24eb6eaab7703713050
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-11-12 18:18:25 -0600

    refactoring

--- extras/guildCreate.js
+++
@@ -1,5 +0,0 @@
-const config = require(`@root/config.json`);
-exports.run = (client, guild) => {
-    console.log(`${client.user.username} has been removed from: ${guild.name} (id: ${guild.id})`);
-	client.user.setActivity(`${client.guilds.size} Servers | ${config.prefix}help`, {type: 'WATCHING'});
-};
--- extras/guildDelete.js
+++
@@ -1,5 +0,0 @@
-const config = require(`@root/config.json`);
-exports.run = (client, guild) => {
-    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-	client.user.setActivity(`${client.guilds.size} Servers | ${config.prefix}help`, {type: 'WATCHING'});
-};
