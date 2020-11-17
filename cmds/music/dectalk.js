4810f07ff09193f78d9dcda7325ac72afd340a9b
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-11-17 10:41:01 -0600

    Update dectalk.js

--- cmds/music/dectalk.js
+++ cmds/music/dectalk.js
@@ -49,21 +49,13 @@ module.exports = class DECTalkCommand extends Command {
       return message.reply(
         ':no_entry: Please join a voice channel and try again!'
       );
-		if (
-			typeof message.guild.musicData.songDispatcher == 'undefined' ||
-			message.guild.musicData.songDispatcher == null
-		  ) {
-			return message.say(':x: There is no song playing right now!');
-		  } else if (voiceChannel.id !== message.guild.me.voice.channel.id) {
-			message.reply(
-			  `:no_entry: You must be in the same voice channel as the bot's in order to use that!`
-			);
-			return;
-		  }
-	  
-		  message.say(':pause_button: Song was paused!');
+		if (typeof message.guild.musicData.songDispatcher !== 'undefined' ||
+			message.guild.musicData.songDispatcher !== null
+		  ) {   message.say(':pause_button: Song was paused!');
 	  
 		  message.guild.musicData.songDispatcher.pause();
+		}
+		
 		const connection = this.client.voice.connections.get(message.guild.id);
 		if (!connection) {
 			const usage = this.client.registry.commands.get('join').usage();
