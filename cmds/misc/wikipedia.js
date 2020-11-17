c0eca01f462fa7f571c72c172921bc4e2ea1a1c1
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-11-17 12:42:23 -0600

    web stuff

---
+++ cmds/misc/wikipedia.js
@@ -0,0 +1,63 @@
+  
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+const request = require('node-superfetch');
+const { shorten } = require('@util/util');
+
+module.exports = class WikipediaCommand extends Command {
+	constructor(client) {
+		super(client, {
+            name: 'wikipedia',
+            aliases: ['wiki'],
+			group: 'misc',
+			memberName: 'wikipedia',
+			description: 'Searches Wikipedia for your query.',
+			clientPermissions: ['EMBED_LINKS'],
+			credit: [
+				{
+					name: 'Wikipedia',
+					url: 'https://www.wikipedia.org/',
+					reason: 'API',
+					reasonURL: 'https://en.wikipedia.org/w/api.php'
+				}
+			],
+			args: [
+				{
+					key: 'query',
+					prompt: 'What article would you like to search for?',
+					type: 'string'
+				}
+			]
+		});
+	}
+
+	async run(msg, { query }) {
+		try {
+			const { body } = await request
+				.get('https://en.wikipedia.org/w/api.php')
+				.query({
+					action: 'query',
+					prop: 'extracts|pageimages',
+					format: 'json',
+					titles: query,
+					exintro: '',
+					explaintext: '',
+					pithumbsize: 150,
+					redirects: '',
+					formatversion: 2
+				});
+			const data = body.query.pages[0];
+			if (data.missing) return msg.say('Could not find any results.');
+			const embed = new MessageEmbed()
+				.setColor(0xE7E7E7)
+				.setTitle(data.title)
+				.setAuthor('Wikipedia', 'https://i.imgur.com/Z7NJBK2.png', 'https://www.wikipedia.org/')
+				.setThumbnail(data.thumbnail ? data.thumbnail.source : null)
+				.setURL(`https://en.wikipedia.org/wiki/${encodeURIComponent(query).replace(')', '%29')}`)
+				.setDescription(shorten(data.extract.replace('\n', '\n\n')));
+			return msg.embed(embed);
+		} catch (err) {
+			return msg.reply(`Oh no, an error occurred: \`${err.message}\`. Try again later!`);
+		}
+	}
+};
