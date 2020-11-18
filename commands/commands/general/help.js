5b3c95d30dd30c4f3ff9c69e3edc57fb9be6796e
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-11-18 11:01:42 -0600

    Update help.js

--- commands/commands/general/help.js
+++ commands/commands/general/help.js
@@ -28,7 +28,7 @@ module.exports = {
                   // A MessageEmbed to actually send in chat, and...
                   content: new Discord.MessageEmbed({
                       title: 'Utility Commands',
-                      description: '**sys**, Sysinfo.exe \n **help (h)**, Shows this menu \n **msgdel**, Message Genocide \n **ping**, Find your ping to me! \n **summon**, *Holy Music stops* \n **country**, Gets info about the specified country \n **math**, Can solve math problems \n **element**, Gets the info about said element on the preiodic table \n **translate**, Translates text between languages \n **wikia (fandom)**, Search a Wikia wiki \n **wikipedia (wiki)**, Gets a summary about said topic \n '
+                      description: '**sys**, Sysinfo.exe \n **help (h)**, Shows this menu \n **msgdel**, Message Genocide \n **ping**, Find your ping to me! \n **summon**, *Holy Music stops* \n **country**, Gets info about the specified country \n **math**, Can solve math problems \n **element**, Gets the info about said element on the preiodic table \n **translate**, Translates text between languages (ex. en es hello)\n **wikia (fandom)**, Search a Wikia wiki \n **wikipedia (wiki)**, Gets a summary about said topic \n '
                    
                       
                   }),
@@ -43,7 +43,7 @@ module.exports = {
                 name: 'fun',
                 content: new Discord.MessageEmbed({
                     title: 'Fun Commands',
-                    description: `**add**, Adds two numbers \n  **reddit (r)**, *Taste the memes* \n **news (n)**, Shows some news articles \n **weather (w)**, Shows the weather for your city \n **todayinhistory (tih)**, Finds an event that happened on specific date. \n **urbandictionary (ud)**, Looks up a word/phrase. \n **giphy (gif)**, Gets a gif from Giphy \n **hangman**, Play hangman with over 50,000 words \n **imposter**, Play a game of Imposter with a group of people \n **tictctoe (ttt)**, Play a game of tic tac toe with someone \n `
+                    description: `**add**, Adds two numbers \n  **reddit (r)**, *Taste the memes* \n **news (n)**, Shows some news articles \n **weather (w)**, Shows the weather for your city \n **todayinhistory (tih)**, Finds an event that happened on specific date (ex. 4 13) \n **urbandictionary (ud)**, Looks up a word/phrase. \n **giphy (gif)**, Gets a gif from Giphy \n **hangman**, Play hangman with over 50,000 words \n **imposter**, Play a game of Imposter with a group of people \n **tictctoe (ttt)**, Play a game of tic tac toe with someone \n `
                 }),
                 reactions: {
                     '◀': 'general',
