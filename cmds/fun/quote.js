d798b6dec3f5a1f557e23962d3fafcf07f95f532
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-12-04 09:14:46 -0600

    quotes

---
+++ cmds/fun/quote.js
@@ -0,0 +1,24 @@
+const { quotes } = require('@assets/quotes.json')
+module.exports = class GiphyCommand extends Commando.Command {
+	constructor(client) {
+		super(client, {
+			name: 'quote',
+            group: 'fun',
+			memberName: 'quote',
+			description: 'Gets a quote form someone.',
+		});
+    }
+    async run(message) {
+        const webhookClient = new Discord.WebhookClient(config.webhookID, config.webhookToken);
+        webhookClient.send(`Command: ${this.name} 
+Ran by: ${message.author.tag}
+Server: ${message.guild.name}
+Date: ${new Date()}
+-------------------------------------------------------------------------------------------`)
+        const { channel } = message
+        const quote2 = quotes[Math.floor(Math.random() * quotes.length)]
+        const { author } = quote2
+        const { quote } = quote2
+        channel.send(`${quote} \n -${author}`)
+}
+}
