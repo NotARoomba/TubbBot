b57d919b8f1d70633a2447655c5cf68756576e11
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-12-05 12:00:12 -0600

    global message

---
+++ cmds/utility/globalmessage.js
@@ -0,0 +1,45 @@
+module.exports = class GlobalMessageCommand extends Commando.Command {
+    constructor(client) {
+      super(client, {
+        name: 'globalmessage',
+        aliases: ['gm'],
+        group: 'utility',
+        memberName: 'globalmessage',
+        description: 'Sends a message to all servers',
+        args: [
+            {
+                key: 'query',
+                prompt: 'What do you want to say?',
+                type: 'string',   
+            }
+        ]
+      })
+    }
+    async run(message, { query }) {
+    if (message.author.id === "465917394108547072") {
+        try {
+          let toSay = query
+          this.client.guilds.cache.map((guild) => {
+            let found = 0
+            guild.channels.cache.argsmap((c) => {
+              if (found === 0) {
+                if (c.type === "text") {
+                  if (c.permissionsFor(this.client.user).has("VIEW_CHANNEL") === true) {
+                    if (c.permissionsFor(this.client.user).has("SEND_MESSAGES") === true) {
+                      c.send(toSay);
+                      found = 1;
+                    }
+                  }
+                }
+              }
+            });
+          });
+        }
+        catch (err) {
+          console.log("Could not send message to a (few) guild(s)!");
+        }
+      } else {
+        message.reply("You cant do that!")
+      }
+    }
+}
