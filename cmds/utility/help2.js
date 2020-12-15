5489f4db14735677a4a21e5e93e2c8b9aa46a99e
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-12-14 19:43:23 -0600

    help stuff 2

--- cmds/utility/help2.js
+++ cmds/utility/help2.js
@@ -1,5 +1,6 @@
 const { CommandoRegistry } = require("discord.js-commando");
 
+
 module.exports = class Help2Command extends Commando.Command {
     constructor(client) {
         super(client, {
@@ -11,22 +12,37 @@ module.exports = class Help2Command extends Commando.Command {
         });
     }
     async run(message) {
-        const commands = Commando.commands
-        console.log(commands)
+        fs.readdir("./cmds", (error, folders) => {
 
-        console.log(command)
-        const commandEmbed = new Pagination.FieldsEmbed()
-            .setArray(command)
-            .setAuthorizedUsers([message.author.id])
-            .setChannel(message.channel)
-            .setElementsPerPage(10)
-            .formatField('# - Command', function (e) {
-                return `**${command.indexOf(e) + 1}**: ${e.name}`;
+            folders.forEach(folder => {
+                fs.readdir(`./cmds/${folder}/`, (error, possibleCommands) => {
+     
+                   let jsCommands = possibleCommands.filter(pcmd => pcmd.split(".").pop() === "js");
+                   if (jsCommands.length <= 0) {
+                         console.log(`No commands to load in ${folder}!`);
+                         return;
+                   }
+     
+                   jsCommands.forEach(command => {
+                        let props = require(`../${folder}/${command}`);
+                        let name = props.name.replace("Command", "").toLowerCase();
+                        const description = props.description;
+                        console.log(`Name: ${name}, description: ${description}.`); //Name: undefined, description: undefined.
+                   });
+                })
             });
+        });
+        // const commandEmbed = new Pagination.FieldsEmbed()
+        //     .setArray(command)
+        //     .setAuthorizedUsers([message.author.id])
+        //     .setChannel(message.channel)
+        //     .setElementsPerPage(10)
+        //     .formatField('# - Command', function (e) {
+        //         return `**${command.indexOf(e) + 1}**: ${e.name}`;
+        //     });
 
-        commandEmbed.embed.setColor('#ff7373').setTitle('Help');
-        commandEmbed.build();
-
+        // commandEmbed.embed.setColor('#ff7373').setTitle('Help');
+        // commandEmbed.build();
 
     }
 }
