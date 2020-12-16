c76b400de6532855e5eaac8d40cb4cbc5647d6a6
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-12-16 15:26:17 -0600

    remname client

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ global.serverSchema = require('@schemas/server-schema')
 global.muteSchema = require('@schemas/mute-schema')
 global.Canvas = require('canvas')
 global.math = require('mathjs');
-const Client = require('@util/Client');
+const Client = require('@util/client');
 global.client = new Client({
   commandPrefix: process.env.PREFIX,
   owner: process.env.OWNERS,
