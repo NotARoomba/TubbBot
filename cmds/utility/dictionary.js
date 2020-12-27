a68878e7569b8333c4b090daea7d8c0bce6f452c
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2020-12-26 23:26:31 -0600

    dic

---
+++ cmds/utility/dictionary.js
@@ -0,0 +1,36 @@
+module.exports = class DictionaryCommand extends Commando.Command {
+	constructor(client) {
+		super(client, {
+            name: 'dictionary',
+            aliases: ['d'],
+			group: 'util',
+			memberName: 'dictionary',
+			description: 'Responds with information on a word.',
+			args: [
+				{
+					key: 'word',
+					prompt: 'What word would you like to search for?',
+					type: 'string',
+				}
+			]
+		});
+	}
+
+	async run(message, { word }) {
+        try {
+			const { body } = await request
+				.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}`)
+				.query({ key: WEBSTER_KEY });
+			if (!body.length) return null;
+			const data = body[0];
+			if (typeof data === 'string') return null;
+			return {
+				name: data.meta.stems[0],
+				partOfSpeech: data.fl,
+				definiton: data.shortdef.map((definition, i) => `(${i + 1}) ${definition}`).join('\n')
+			};
+		} catch (err) {
+			return null;
+		}
+    }
+}
