c043c82e6f9d821a1bcd350f409849a7cde8496c
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2021-01-19 10:03:15 -0600

    Revert "aef"
    This reverts commit 11b2a43627495a520cfdfb608a92c06e92e107b8.

--- cmds/music/skipto.js
+++ cmds/music/skipto.js
@@ -53,7 +53,7 @@ module.exports = class SkipToCommand extends Commando.Command {
       message.guild.musicData.queue.splice(0, songNumber - 1);
       message.guild.musicData.loopSong = false;
       message.guild.musicData.songDispatcher.end();
-      message.react(":ok_hand:");
+      message.react("👌");
     } else if (message.guild.musicData.loopQueue) {
       const slicedBefore = message.guild.musicData.queue.slice(
         0,
@@ -62,7 +62,7 @@ module.exports = class SkipToCommand extends Commando.Command {
       const slicedAfter = message.guild.musicData.queue.slice(songNumber - 1);
       message.guild.musicData.queue = slicedAfter.concat(slicedBefore);
       message.guild.musicData.songDispatcher.end();
-      message.react(":ok_hand:");
+      message.react("👌");
     }
   }
 };
