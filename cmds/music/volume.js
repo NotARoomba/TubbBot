4c07ea028eb7a50792b20203147cd00902842189
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2021-02-23 12:05:45 -0600

    volume fix

--- cmds/music/volume.js
+++ cmds/music/volume.js
@@ -5,9 +5,9 @@ module.exports = {
     aliases: ['v', 'vol'],
     description: 'Adjust song volume!',
     async execute(message, args, client) {
-        let queue = client.player.getQueue(message);
+        client.player.getQueue(message);
         if (isNaN(Number(args))) return message.reply(`that is not a number.`);
-        queue.setVolume(message, args);
+        client.player.setVolume(message, Number(args));
         message.channel.send(`Changed the volume to ${args}%`);
     }
 }
