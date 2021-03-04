f9f1601b050631ddef689b349564496722952f7e
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2021-03-04 11:49:08 -0600

    mhm

--- cmds/chess/draw.js
+++ cmds/chess/draw.js
@@ -7,11 +7,12 @@ module.exports = {
     async execute(message, args, client) {
         if (!await inGame(message, message.author, client)) return message.reply(`you are not in a game.`)
         try {
+            const [a] = await client.pool.query(`SELECT * FROM chessGames WHERE guild = ${message.guild.id} AND p1 = ${message.author.id} or p2 = ${message.author.id} AND current = 1`)
             let accepted;
             await message.react('✅')
             await message.react('❌')
             const filter = (reaction, b) => {
-                return ['✅', '❌'].includes(reaction.emoji.name) && b.id.includes(user.id);
+                return ['✅', '❌'].includes(reaction.emoji.name) && b.id.includes(a[0].p2);
             };
             const collector = await message.createReactionCollector(filter, { time: 30000 });
             collector.on('collect', (reaction) => {
@@ -27,9 +28,9 @@ module.exports = {
                 if (collected.size == 0) message.channel.send(`Looks like they declined the draw offer...`)
             });
             if (accepted == false) return
-            const [a] = await client.pool.query(`SELECT * FROM chessGames WHERE guild = ${message.guild.id} AND p1 = ${message.author.id} or p2 = ${message.author.id} AND current = 1`)
-            message.channel.send(`Draw agreed to between <@${a[0].p1}> and <@${a[0].p2}>!`);
-            message.channel.send(`http://www.jinchess.com/chessboard/?p=${encodeURI(a[0].fen)}&s=l&dsc=%23b58863&lsc=%23f0d9b5&ps=merida&cm=o`)
+            const [b] = await client.pool.query(`SELECT * FROM chessGames WHERE guild = ${message.guild.id} AND p1 = ${message.author.id} or p2 = ${message.author.id} AND current = 1`)
+            message.channel.send(`Draw agreed to between <@${b[0].p1}> and <@${b[0].p2}>!`);
+            message.channel.send(`http://www.jinchess.com/chessboard/?p=${encodeURI(b[0].fen)}&s=l&dsc=%23b58863&lsc=%23f0d9b5&ps=merida&cm=o`)
             endChessGame(message, client, b[0].p1, b[0].p2, .5, true)
         } catch (err) {
             console.log(err)
