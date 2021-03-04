7c4bf5283566bd1d82730131703f1ed6e3cf9620
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2021-03-04 12:23:02 -0600

    async and draw

--- cmds/chess/draw.js
+++ cmds/chess/draw.js
@@ -8,13 +8,14 @@ module.exports = {
         if (!await inGame(message, message.author, client)) return message.reply(`you are not in a game.`)
         try {
             const [a] = await client.pool.query(`SELECT * FROM chessGames WHERE guild = ${message.guild.id} AND p1 = ${message.author.id} or p2 = ${message.author.id} AND current = 1`)
-            await message.react('✅')
-            await message.react('❌')
+            let mesg = message.channel.send(`<@${a[0].p2}>, <@${a[0].p1}> is offering a draw, do you accept?`)
+            await mesg.react('✅')
+            await mesg.react('❌')
             const filter = (reaction, b) => {
                 return ['✅', '❌'].includes(reaction.emoji.name) && b.id.includes(a[0].p2);
             };
-            const collector = await message.createReactionCollector(filter, { time: 30000 });
-            collector.on('collect', (reaction) => {
+            const collector = await mesg.createReactionCollector(filter, { time: 30000 });
+            collector.on('collect', async (reaction) => {
                 if (reaction.emoji.name === '✅') {
                     const [b] = await client.pool.query(`SELECT * FROM chessGames WHERE guild = ${message.guild.id} AND p1 = ${message.author.id} or p2 = ${message.author.id} AND current = 1`)
                     message.channel.send(`Draw agreed to between <@${b[0].p1}> and <@${b[0].p2}>!`);
