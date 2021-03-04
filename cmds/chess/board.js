611b218fd7cf4b3d9fca346a7cf81a949890cc2c
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2021-03-03 21:06:32 -0600

    better board showing and checkmate

--- cmds/chess/board.js
+++ cmds/chess/board.js
@@ -1,3 +1,4 @@
+const { Chess } = require('chess.js');
 const { inGame, getBoardImage } = require('../../function');
 module.exports = {
     name: 'board',
@@ -8,6 +9,9 @@ module.exports = {
     async execute(message, args, client) {
         if (!await inGame(message, message.author, client)) return message.reply(`you are not in a game.`)
         const [game] = await client.pool.query(`SELECT * FROM chessGames WHERE guild = ${message.guild.id} AND p1 = ${message.author.id} or p2 = ${message.author.id} AND current = 1`)
-        message.channel.send(await getBoardImage(game[0].fen))
+        let fen = game[0].fen
+        const chess = new Chess(fen)
+        message.channel.send(chess.turn() === 'w' ? `White to move...` : `Black to move...`)
+        message.channel.send(`http://www.jinchess.com/chessboard/?p=${encodeURI(game[0].fen)}&s=l&dsc=%23b58863&lsc=%23f0d9b5&ps=merida&cm=o`)
     }
 }
