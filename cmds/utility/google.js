483afb193be01872c6e0e48ae36d704c7ef71e45
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2021-03-09 07:31:48 -0600

    got gooel colors

---
+++ cmds/utility/google.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const googleIt = require('google-it');
+module.exports = {
+    name: 'google',
+    group: 'utility',
+    usage: `google (query)`,
+    aliases: ['g'],
+    description: `Searches google for something!`,
+    async execute(message, args, client) {
+        const colors = ['4285f4', 'ea4335', 'fbbc05', '34a853']
+        googleIt({ 'query': args }).then(results => {
+            for (let i = 0; i < 5; i++) {
+                const element = results[i];
+                const embed = new Discord.MessageEmbed()
+                    .setTitle(element.title)
+                    .setURL(element.link)
+                    .setColor(colors[Math.floor(Math.random() * colors.length)])
+                    .setDescription(element.snippet)
+                message.channel.send(embed)
+            }
+        }).catch(e => { })
+    }
+}
