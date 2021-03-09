e8d6dcbc6c9e20ae06670d88ccf47f26554d5814
L061571C5 <70660308+L061571C5@users.noreply.github.com>
2021-03-09 14:04:16 -0600

    better help menu config

--- cmds/utility/help.js
+++ cmds/utility/help.js
@@ -1,10 +1,7 @@
 const Discord = require('discord.js');
 var read = require('fs-readdir-recursive')
-const { searchArray, defaultEmbed } = require('../../function.js')
-let utility = []
-let music = []
-let chess = []
-let nsfw = []
+const { searchForCommand, defaultEmbed } = require('../../function.js')
+const commands = []
 module.exports = {
     name: 'help',
     group: 'utility',
@@ -17,79 +14,54 @@ module.exports = {
                 .setTitle(`Please Specify`)
                 .setColor('#dbc300')
                 .addFields([
-                    {
-                        name: 'Music Commands', value: `${prefix[0].prefix}help Music`
-                    },
-                    {
-                        name: 'Utility Commands', value: `${prefix[0].prefix}help Utility`
-                    },
-                    {
-                        name: 'Chess Commands', value: `${prefix[0].prefix}help Chess`
-                    },
+                    { name: 'Music Commands', value: `${prefix[0].prefix}help Music` },
+                    { name: 'Utility Commands', value: `${prefix[0].prefix}help Utility` },
+                    { name: 'Chess Commands', value: `${prefix[0].prefix}help Chess` },
                 ])
                 .addFields(message.channel.nsfw ?
-                    [{
-                        name: 'NSFW Commands (requires an NSFW channel)', value: `${prefix[0].prefix}help NSFW`
-                    }, {
-                        name: 'Information on a Command', value: `${prefix[0].prefix}help [command]`
-                    }
-                    ] :
-                    {
-                        name: 'Information on a Command', value: `${prefix[0].prefix}help [command]`
-                    })
-
+                    [{ name: 'NSFW Commands (requires an NSFW channel)', value: `${prefix[0].prefix}help NSFW` },
+                    { name: 'Information on a Command', value: `${prefix[0].prefix}help [command]` }
+                    ] : { name: 'Information on a Command', value: `${prefix[0].prefix}help [command]` })
             return message.channel.send(embed)
         }
         let cmdarr = read('./cmds')
         cmdarr.forEach(e => {
             let cmd = e.replace(`\\`, '/')
             let cmdpath = require(`../${cmd}`)
-            if (cmdpath.group == 'music') {
-                music.push({ name: cmdpath.name, description: cmdpath.description, aliases: cmdpath.aliases, usage: cmdpath.usage })
-            } else if (cmdpath.group == 'utility') {
-                utility.push({ name: cmdpath.name, description: cmdpath.description, aliases: cmdpath.aliases, usage: cmdpath.usage })
-            } else if (cmdpath.group == 'chess') {
-                chess.push({ name: cmdpath.name, description: cmdpath.description, aliases: cmdpath.aliases, usage: cmdpath.usage })
-            } else if (cmdpath.group == 'NSFW') {
-                nsfw.push({ name: cmdpath.name, description: cmdpath.description, aliases: cmdpath.aliases, usage: cmdpath.usage, NSFW: cmdpath.NSFW })
-            }
+            commands.push(cmdpath)
         });
-        let total = music.concat(utility, chess, nsfw)
-        if (args == 'Music') {
-            defaultEmbed(message, music, 'Music', client)
-            music = []
-        } else if (args == 'Utility') {
-            defaultEmbed(message, utility, 'Utility', client)
-            utility = []
-        } else if (args == 'Chess') {
-            defaultEmbed(message, chess, 'Chess', client)
-            chess = []
-        } else if (args == 'NSFW') {
-            if (message.channel.nsfw !== true) return message.reply(`move it to an NSFW channel.`)
-            defaultEmbed(message, nsfw, 'NSFW', client)
-            nsfw = []
-        }
-        else {
-            args = args.toLowerCase()
-            const cmd = searchArray(args, total)
-            if (cmd) {
-                if (cmd.NSFW == true && message.channel.nsfw !== true) return message.reply(`move it to an NSFW channel.`)
-                var embed = new Discord.MessageEmbed()
-                    .setTitle(cmd.name)
-                    .setDescription(cmd.description)
-                    .addFields([
-                        {
-                            name: 'Usage', value: `${prefix[0].prefix}${cmd.usage}`
-                        },
-                        {
-                            name: 'Aliases', value: `${cmd.aliases}`
-                        },
-                        {
-                            name: 'NSFW', value: `${cmd.NSFW == true ? true : false}`
-                        }
-                    ])
-            } else return message.reply('that is not a valid command name.')
-            return message.channel.send(embed)
+        switch (args) {
+            case 'Music':
+                defaultEmbed(message, commands, 'Music', client, 'music')
+                break;
+            case 'Chess':
+                defaultEmbed(message, commands, 'Chess', client, 'chess')
+                break;
+            case 'Utility':
+                defaultEmbed(message, commands, 'Utility', client, 'utility')
+                break;
+            case 'NSFW':
+                if (message.channel.nsfw !== true) return message.reply(`move it to an NSFW channel.`)
+                defaultEmbed(message, commands, 'NSFW', client, 'NSFW')
+                break;
+
+            default:
+                args = args.toLowerCase()
+                const cmd = searchForCommand(args, commands)
+                if (cmd) {
+                    if (cmd.NSFW == true && message.channel.nsfw !== true) return message.reply(`move it to an NSFW channel.`)
+                    var embed = new Discord.MessageEmbed()
+                        .setTitle(cmd.name)
+                        .setDescription(cmd.description)
+                        .setColor('#dbc300')
+                        .addFields([
+                            { name: 'Usage', value: `${prefix[0].prefix}${cmd.usage}` },
+                            { name: 'Aliases', value: `${cmd.aliases}` },
+                            { name: 'NSFW', value: `${cmd.NSFW == true ? true : false}` }
+                        ])
+                    message.channel.send(embed)
+                } else return message.reply('that is not a valid command name.')
+                break;
         }
     }
 }
