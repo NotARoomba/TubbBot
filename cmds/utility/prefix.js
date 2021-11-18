1fbb21825f016d99bb83e48249e0a17b3275251d
Nathan Von Braun <70660308+NotARoomba@users.noreply.github.com>
2021-11-18 17:58:55 +0000

    final fix for database?

--- cmds/utility/prefix.js
+++ cmds/utility/prefix.js
@@ -5,6 +5,7 @@ module.exports = {
     permissions: ['ADMINISTRATOR'],
     description: 'Changes the prefix for your server.',
     async execute(message, args, client) {
+				return message.client.reply("This command isn't working currently...")
         if (args.length === 0 || args === "") return message.reply('that is not a valid prefix.')
         await client.pool.query(`UPDATE servers SET prefix = '${args}' WHERE id = ${message.guild.id}`);
         message.channel.send(`Prefix updated to \`${args}\``)
