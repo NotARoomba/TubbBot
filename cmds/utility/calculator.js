3bc2f3e695337a4420b233cf46ebb6c2932d0bfe
Nathan Von Braun <70660308+NotARoomba@users.noreply.github.com>
2021-12-07 20:36:20 +0000

    wolfram alpha stuff

--- cmds/utility/calculator.js
+++ cmds/utility/calculator.js
@@ -3,7 +3,6 @@ var math = require('mathjs')
 const Pagination = require('discord-paginationembed');
 const WolframAlphaAPI = require('wolfram-alpha-api');
 const waApi = WolframAlphaAPI(process.env.WOLFRAM);
-//https://products.wolframalpha.com/api/libraries/javascript/
 module.exports = {
 	name: "calculator",
 	group: 'utility',
@@ -12,6 +11,27 @@ module.exports = {
 	description: 'Get the answer to a math problem.',
 	async execute(message, args) {
 		try {
+			const embeds = [];
+			await waApi.getFull(args).then((queryresult) => {
+				const pods = queryresult.pods;
+				const output = pods.map((pod) => {
+					const subpodContent = pod.subpods.map(subpod => subpod.img.src)
+					const embed = new Discord.MessageEmbed()
+						.setColor('#ff0000')
+						.setTitle(pod.title)
+						.setAuthor('Wolfram Alpha', 'https://www.wolframalpha.com/_next/static/images/share_3G6HuGr6.png', 'https://www.wolframalpha.com/')
+						.setImage(subpodContent[0])
+						.setTimestamp()
+						.setFooter("Powered by Wolfram Alpha Api", message.client.user.displayAvatarURL());
+					embeds.push(embed)
+				})
+			}).catch();
+			const embed = new Pagination.Embeds()
+				.setArray(embeds)
+				.setAuthorizedUsers([message.author.id])
+				.setChannel(message.channel)
+				.setPageIndicator('footer', (page, pages) => `Page ${page} of ${pages}`)
+			return await embed.build()
 			/*
 			waApi.getSimple(args).then((url) => {
 				const buffer = new Buffer.from(url.split(",")[1], "base64");
@@ -28,34 +48,10 @@ module.exports = {
 			}).catch(console.error);
 			//return await math.evaluate(equation);
 			*/
-			const data = await waApi.getShort(args)
-			message.channel.send(`\`\`\`${data}\`\`\``)
+			//const data = await waApi.getShort(args)
+			//message.channel.send(`\`\`\`${data}\`\`\``)
 		} catch (err) {
-			try {
-				const embeds = [];
-				await waApi.getFull(args).then((queryresult) => {
-					const pods = queryresult.pods;
-					const output = pods.map((pod) => {
-						const subpodContent = pod.subpods.map(subpod => subpod.img.src)
-						const embed = new Discord.MessageEmbed()
-							.setColor('#ff0000')
-							.setTitle(pod.title)
-							.setAuthor('Wolfram Alpha', 'https://www.wolframalpha.com/_next/static/images/share_3G6HuGr6.png', 'https://www.wolframalpha.com/')
-							.setImage(subpodContent[0])
-							.setTimestamp()
-							.setFooter("Powered by Wolfram Alpha Api", message.client.user.displayAvatarURL());
-						embeds.push(embed)
-					})
-				}).catch();
-				const embed = new Pagination.Embeds()
-					.setArray(embeds)
-					.setAuthorizedUsers([message.author.id])
-					.setChannel(message.channel)
-					.setPageIndicator('footer', (page, pages) => `Page ${page} of ${pages}`)
-				return await embed.build()
-			} catch (err) {
-				message.reply(`\`\`\`${args}\`\`\` is not a valid query.`)
-			}
+			message.reply(`\`\`\`${args}\`\`\` is not a valid query.`)
 		}
 	}
 }
