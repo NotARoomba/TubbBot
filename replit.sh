619613a35ce333f1a5a032f093f8b0832ef49434
Nathan Von Braun <70660308+NotARoomba@users.noreply.github.com>
2021-12-11 06:12:58 +0000

    yes

--- replit.sh
+++ replit.sh
@@ -1,8 +1,9 @@
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 unset PREFIX &&
 export NVM_DIR="/config/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  &&# This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  &&
 
 nvm install 14.16.1 &&
-nvm use 14.16.1 
+nvm use 14.16.1 &&
+node index.js
