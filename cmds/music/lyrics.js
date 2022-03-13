a76029cb3b29afe21bf9e9fc7b1f9407b6e2a0c2
NotARoomba1 <70660308+NotARoomba@users.noreply.github.com>
2022-03-13 02:07:58 +0000

    lyrics fix

--- cmds/music/lyrics.js
+++ cmds/music/lyrics.js
@@ -8,20 +8,25 @@ module.exports = {
 	description: 'Get the lyrics for the currently playing song or query!',
 	async execute(message, songName) {
 		if (!message.guild.musicData.isPlaying && songName == '') return message.reply(`please play a song or input your search query.`)
+    let songArtist;
 		if (songName == '' && message.guild.musicData.isPlaying) {
 			const track = message.guild.musicData.nowPlaying
 			songName = track.title
+      artist = track.artist
 		}
 		songName = songName.replace(/ *\([^)]*\) */g, '');
 		songName = songName.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '');
+    songArtist = songName.replace(/ *\([^)]*\) */g, '');
+		songArtist = songName.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '');
 		const options = {
 			apiKey: process.env.GENIUS_LYRICS_API,
 			title: songName,
-			artist: '',
+			artist: songArtist,
 			optimizeQuery: true
 		};
 		const sentMessage = await message.channel.send(':mag: :notes: Searching for lyrics!');
 		getLyrics(options).then((lyrics) => {
+      try {
 			if (lyrics.length > 4095) {
 				message.channel.send('Lyrics are too long to be returned in a message embed!');
 				return;
@@ -48,6 +53,9 @@ module.exports = {
 				message.channel.send(secondLyricsEmbed);
 				return;
 			}
+    } catch (err) {
+         message.channel.send("There are no lyrics for that song!");
+    }
 		});
 	}
 }
